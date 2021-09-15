e30833b0fd8593db10457bbb914d8ed834c0f123
shahbaz <shahbazrock0321@gmail.com>
2021-09-15 15:25:16 +0500

    Make userLogin component and import UserNavbar component

---
+++ components/userLogin/UserLogin.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import React from "react";
+import UserNavbar from "../common/userNavbar/UserNavbar";
+
+const UserLogin: NextPage = () => {
+  return (
+    <div>
+      <UserNavbar />
+    </div>
+  );
+};
+
+export default UserLogin;
