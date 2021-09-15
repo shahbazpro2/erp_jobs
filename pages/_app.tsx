6889c3984db55f248c67b4f48271c2d2b291c161
shahbaz <shahbazrock0321@gmail.com>
2021-09-15 15:24:24 +0500

    Wrap components to theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@material-ui/core'
+import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}>
+
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
 export default MyApp
