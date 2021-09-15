1d9399db83a78a56f1514a4a1c15aaf79e3bd370
shahbaz <shahbazrock0321@gmail.com>
2021-09-15 17:00:25 +0500

    Make stepper

---
+++ components/common/stepper/HorizontalStepper.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+
+const useStyles = makeStyles((theme) => ({
+    label: {
+        fontSize: 17,
+        fontWeight: 'bold'
+    },
+    backButton: {
+        marginRight: theme.spacing(1),
+    },
+    instructions: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    },
+}));
+
+
+interface Props {
+    steps: string[],
+    activeStep: number
+}
+
+
+export default function HorizontalStepper({ steps, activeStep }: Props) {
+    const classes = useStyles();
+    return (
+        <div >
+            <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel >
+                            <div className={classes.label}>
+                                {label}
+                            </div>
+                        </StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+
+        </div>
+    );
+}
