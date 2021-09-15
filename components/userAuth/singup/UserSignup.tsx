113b67ae9d8fdfc7c3a558a85bde5572242e869a
shahbaz <shahbazrock0321@gmail.com>
2021-09-15 17:46:47 +0500

    Make user signup file

---
+++ components/userAuth/singup/UserSignup.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import UserNavbar from "../../common/userNavbar/UserNavbar";
+import Stepper from "./stepper/Stepper";
+
+const UserSignup = () => {
+  return (
+    <div className="bg-[#F2F2F2]">
+      <UserNavbar />
+      <div className="container mx-auto">
+        <div className="flex justify-center items-center h-screen">
+          <div className="w-full">
+            <Stepper />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserSignup;
