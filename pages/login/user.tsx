ff90ff1ef6a4d6ca5316b508b1829e2f2db91a44
shahbaz <shahbazrock0321@gmail.com>
2021-09-15 15:26:11 +0500

    Make user login page

---
+++ pages/login/user.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import UserLogin from "../../components/userLogin/UserLogin";
+
+const user: NextPage = () => {
+  return <UserLogin />;
+};
+
+export default user;
