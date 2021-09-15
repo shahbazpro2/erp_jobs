51937eb6f79afdd16f501b14b1106e3953ec02fa
shahbaz <shahbazrock0321@gmail.com>
2021-09-15 18:43:05 +0500

    Add Textfields and buttons

--- components/userAuth/singup/stepper/stepperContent/step1/Step1.tsx
+++ components/userAuth/singup/stepper/stepperContent/step1/Step1.tsx
@@ -1,3 +1,4 @@
+import { Button, NoSsr, TextField } from '@material-ui/core'
 import React from 'react'
 import AuthWrapper from '../../../../../common/authWrapper/AuthWrapper'
 
@@ -6,7 +7,40 @@ const Step1 = () => {
         <div className="grid grid-cols-6 justify-center">
             <div className="col-start-3 col-span-2">
                 <AuthWrapper>
-                    login
+                    <div className="mt-6">
+                        <div className="grid gap-5">
+                            <TextField
+                                id="outlined-name"
+                                label="Your Name"
+                                variant="outlined"
+                                className="w-full"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <TextField
+                                id="outlined-name"
+                                label="Your Email"
+                                variant="outlined"
+                                className="w-full"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <TextField
+                                id="outlined-name"
+                                label="Enter Password"
+                                variant="outlined"
+                                className="w-full"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <Button variant="contained" color="primary" disableElevation>
+                                Continue
+                            </Button>
+                        </div>
+                    </div>
                 </AuthWrapper>
             </div>
         </div>
