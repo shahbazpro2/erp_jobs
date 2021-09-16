4663d3622ac1f229ed09c68ef213d7a74682068f
shahbaz <shahbazrock0321@gmail.com>
2021-09-16 13:17:37 +0500

    Add custom dotted line

--- components/common/stepper/HorizontalStepper.tsx
+++ components/common/stepper/HorizontalStepper.tsx
@@ -3,14 +3,28 @@ import { makeStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import { StepConnector } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+
+const ColorlibConnector = withStyles({
+    alternativeLabel: {
+        top: 17,
+        zIndex: 111
+    },
+
+    line: {
+        border: 'none'
+    },
+})(StepConnector);
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        background: 'transparent'
+        background: 'transparent',
     },
     label: {
         fontSize: 17,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
     backButton: {
         marginRight: theme.spacing(1),
@@ -31,8 +45,10 @@ interface Props {
 export default function HorizontalStepper({ steps, activeStep }: Props) {
     const classes = useStyles();
     return (
-        <div >
-            <Stepper className={classes.root} activeStep={activeStep} alternativeLabel>
+        <div className="max-w-[1100px] relative m-auto">
+
+            <Stepper className={classes.root} activeStep={activeStep} alternativeLabel connector={<ColorlibConnector />}>
+
                 {steps.map((label) => (
                     <Step key={label}>
                         <StepLabel >
@@ -43,7 +59,7 @@ export default function HorizontalStepper({ steps, activeStep }: Props) {
                     </Step>
                 ))}
             </Stepper>
-
+            <div className="dotted-spaced"></div>
         </div>
     );
 }
