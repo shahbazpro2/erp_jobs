340c7ef2f00cc6a7bfe1a0291191501f6551cce5
shahbaz <shahbazrock0321@gmail.com>
2021-09-16 13:18:11 +0500

    Adjust UserSignup page height

--- components/userAuth/singup/UserSignup.tsx
+++ components/userAuth/singup/UserSignup.tsx
@@ -6,8 +6,8 @@ const UserSignup = () => {
   return (
     <div className="bg-[#F2F2F2] ">
       <UserNavbar />
-      <div className="container mx-auto">
-        <div className="flex justify-center items-center min-h-[91vh]">
+      <div className="container mx-auto py-10">
+        <div className="flex justify-center min-h-[83vh]">
           <div className="w-full">
             <Stepper />
           </div>
