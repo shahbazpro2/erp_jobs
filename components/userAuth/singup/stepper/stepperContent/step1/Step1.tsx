a81ff7a88213d021b4fcaa148282c0a1f211f900
shahbaz <shahbazrock0321@gmail.com>
2021-09-16 14:22:04 +0500

    Form submit with error handling

--- components/userAuth/singup/stepper/stepperContent/step1/Step1.tsx
+++ components/userAuth/singup/stepper/stepperContent/step1/Step1.tsx
@@ -1,49 +1,88 @@
 import { Button, TextField } from '@material-ui/core'
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { ChangeEvent, Dispatch, SetStateAction, SyntheticEvent, useState } from 'react'
 import AuthWrapper from '../../../../../common/authWrapper/AuthWrapper'
+import validateEmail from '../../../../../functions/emailValidation'
 
 interface Props {
     setActiveStep: Dispatch<SetStateAction<number>>
 }
 
 const Step1 = ({ setActiveStep }: Props) => {
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+    const [inputError, setInputError] = useState(false)
+    const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target
+        setState({ ...state, [name]: value })
+    }
+
+    const onSubmit = (e: SyntheticEvent) => {
+        e.preventDefault()
+        setInputError(false)
+        const { name, email, password } = state
+        if (!validateEmail(email) || !name || !password) {
+            setInputError(true)
+            return
+        }
+
+        setActiveStep(1)
+    }
     return (
         <div className="grid grid-cols-6 justify-center mt-8">
             <div className="col-start-3 col-span-2">
                 <AuthWrapper>
                     <div className="mt-6">
-                        <div className="grid gap-5">
-                            <TextField
-                                id="outlined-name"
-                                label="Your Name"
-                                variant="outlined"
-                                className="w-full"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
-                            <TextField
-                                id="outlined-name"
-                                label="Your Email"
-                                variant="outlined"
-                                className="w-full"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
-                            <TextField
-                                id="outlined-name"
-                                label="Enter Password"
-                                variant="outlined"
-                                className="w-full"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
-                            <Button variant="contained" color="primary" disableElevation onClick={() => setActiveStep(1)}>
-                                Continue
-                            </Button>
-                        </div>
+                        <form noValidate autoComplete="off" onSubmit={onSubmit}>
+                            <div className="grid gap-5">
+                                <TextField
+                                    error={inputError && !state.name ? true : false}
+                                    required
+                                    id="outlined-name"
+                                    name="name"
+                                    label="Your Name"
+                                    variant="outlined"
+                                    className="w-full"
+                                    onChange={onChangeInput}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <TextField
+                                    error={inputError && !validateEmail(state.email) ? true : false}
+                                    required
+                                    id="outlined-email"
+                                    name="email"
+                                    label="Your Email"
+                                    type="email"
+                                    variant="outlined"
+                                    className="w-full"
+                                    onChange={onChangeInput}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <TextField
+                                    required
+                                    error={inputError && !state.password ? true : false}
+                                    id="outlined-password"
+                                    name="password"
+                                    type="password"
+                                    label="Enter Password"
+                                    variant="outlined"
+                                    className="w-full"
+                                    onChange={onChangeInput}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <Button type="submit" variant="contained" color="primary" disableElevation >
+                                    Continue
+                                </Button>
+                            </div>
+                        </form>
                         <div className="my-6">
                             <div className="divider">
                                 <span>or</span>
