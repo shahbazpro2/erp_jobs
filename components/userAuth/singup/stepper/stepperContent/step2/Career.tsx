ef3b493a3e08ead098bdd619ca783b6f8f5f5cf2
shahbaz <shahbazrock0321@gmail.com>
2021-09-17 16:00:42 +0500

    Make Carreer component

---
+++ components/userAuth/singup/stepper/stepperContent/step2/Career.tsx
@@ -0,0 +1,34 @@
+import { Button, Divider } from '@material-ui/core'
+import React, { Dispatch, SetStateAction } from 'react'
+import BoxWrapper from '../../../../../common/boxWrapper/BoxWrapper'
+import AddIcon from '@material-ui/icons/Add';
+interface Props {
+    setActive: Dispatch<SetStateAction<string>>
+}
+const Career = ({ setActive }: Props) => {
+    return (
+        <div className="grid grid-cols-7 justify-center mt-8">
+            <div className="col-start-3 col-span-3">
+                <BoxWrapper title="Career Journey" subtitle="Add your career journey, you can add multiple careers.">
+                    <div className="mt-7">
+
+                        <Button variant="outlined" className="w-full" color="primary">
+                            Add New <AddIcon style={{ width: 18 }} />
+                        </Button>
+                    </div>
+                    <div className="my-7">
+                        <Divider />
+                    </div>
+                    <Button variant="contained" className="w-full" color="primary" disableElevation >
+                        Continue
+                    </Button>
+                    <div className="mt-5 text-center">
+                        <Button onClick={() => setActive('education')}>Back</Button>
+                    </div>
+                </BoxWrapper>
+            </div>
+        </div>
+    )
+}
+
+export default Career
