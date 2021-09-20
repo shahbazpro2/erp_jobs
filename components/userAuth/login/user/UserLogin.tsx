0d05d91c606a518cebbda1141b3cff8d8b905d15
shahbaz <shahbazrock0321@gmail.com>
2021-09-20 14:19:54 +0500

    Make UserLogin component and page

---
+++ components/userAuth/login/user/UserLogin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserLogin = () => {
+    return (
+        <div>
+            login
+        </div>
+    )
+}
+
+export default UserLogin
---
+++ pages/login/user.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserLogin from '../../components/userAuth/login/user/UserLogin'
+
+const user = () => {
+    return (
+        <UserLogin />
+    )
+}
+
+export default user
