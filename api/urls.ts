97eaaade5cda7dcc8014d3c1dd60ca35ff160674
shahbaz <shahbazrock0321@gmail.com>
2021-09-20 17:46:40 +0500

    Add getUserUrl

--- api/urls.ts
+++ api/urls.ts
@@ -1,3 +1,4 @@
 const hostUrl = 'http://10.104.45.78:8000'
 export const registerUserUrl = `${hostUrl}/register/candidate/`
 export const loginUserUrl = `${hostUrl}/login/candidate/`
+export const getUserUrl = `${hostUrl}/users/me/`
