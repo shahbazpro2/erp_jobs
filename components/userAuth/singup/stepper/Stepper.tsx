90c22680e28a57c9d52b802ab45695542fe3b93a
shahbaz <shahbazrock0321@gmail.com>
2021-09-21 14:12:11 +0500

    Check isUser completed profile

--- components/userAuth/singup/stepper/Stepper.tsx
+++ components/userAuth/singup/stepper/Stepper.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppSelector } from '../../../../redux/Store';
+import Spinner from '../../../common/spinner/Spinner';
 import HorizontalStepper from '../../../common/stepper/HorizontalStepper'
 import Step1 from './stepperContent/step1/Step1';
 import Step2 from './stepperContent/step2/Step2';
 
 const steps = ["Create an account", "Build your profile", "Upload your CV"];
 const Stepper = () => {
+    const { user }: any = useAppSelector(state => state.authReducer)
+    const [loading, setLoading] = useState(true)
     const [activeStep, setActiveStep] = useState(0)
+
+
+    useEffect(() => {
+        if (Object.keys(user).length && !user.isCompleted)
+            setActiveStep(1)
+        setLoading(false)
+    }, [user])
 
     const getStepperContent = () => {
         switch (activeStep) {
@@ -21,10 +32,14 @@ const Stepper = () => {
     }
     return (
         <>
-            <div className="mb-12">
-                <HorizontalStepper activeStep={activeStep} steps={steps} />
-            </div>
-            {getStepperContent()}
+            {loading ? <Spinner /> :
+                <>
+                    <div className="mb-12">
+                        <HorizontalStepper activeStep={activeStep} steps={steps} />
+                    </div>
+                    {getStepperContent()}
+                </>
+            }
         </>
     )
 }
