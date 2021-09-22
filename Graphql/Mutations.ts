a136c1961407eca9b6cbddd0450c6f002766d2ba
shahbaz <shahbazrock0321@gmail.com>
2021-09-22 12:38:43 +0500

    Make mutaion for createCandidate

---
+++ Graphql/Mutations.ts
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client'
+
+export const CreateCandidate = gql`
+    mutation createCandidate(
+        
+            $phone: String!,
+            $gender:CandidateGender!,
+            $dateOfBirth:DateTime!,
+            $address:String!,
+            $nationality:CandidateNationality!,
+            $residenceCountry:[CandidateCountry]!,
+            $city:String!,
+            $jobStatus:CandidateJobStatus!,
+            $profileVisibility:CandidateProfileVisibilty!,
+            $confidential:Boolean!
+        
+    ){
+    createCandidate(
+    input:{
+            phone:$phone,
+            gender:$gender,
+            dateOfBirth:$dateOfBirth,
+            address:$address,
+            nationality:$nationality,
+            residenceCountry:$residenceCountry,
+            city:$city,
+            jobStatus:$jobStatus,
+            profileVisibility:$profileVisibility,
+            confidential:$confidential
+        
+    }){
+        Candidate{
+        id
+        phone
+        gender
+        dateOfBirth
+    }}
+    }
+`
