b819bd608ab43ee6186ea74246723fd966736119
shahbaz <shahbazrock0321@gmail.com>
2021-09-22 17:54:27 +0500

    Make CareerContent component

---
+++ components/userAuth/singup/stepper/stepperContent/step2/career/CareerContent.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import BoxWrapper from '../../../../../../common/boxWrapper/BoxWrapper'
+import ModalWrapper from '../../../../../../common/modals/ModalWrapper'
+
+const CareerContent = () => {
+    return (
+        <>
+            <ModalWrapper>
+
+                <div className="w-[40%] absolute-center">
+                    <BoxWrapper>
+                        hello
+                    </BoxWrapper>
+                </div>
+
+            </ModalWrapper>
+        </>
+    )
+}
+
+export default CareerContent
