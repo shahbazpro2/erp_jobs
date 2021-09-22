11d3d4d9dc9e330b1b5b7cdc4bcd14206d2afabf
shahbaz <shahbazrock0321@gmail.com>
2021-09-22 18:59:21 +0500

    Update ModalContext path and remove onClose

--- components/common/modals/ModalWrapper.tsx
+++ components/common/modals/ModalWrapper.tsx
@@ -1,6 +1,6 @@
 import { Modal } from '@mui/material'
 import React, { ReactNode, useContext } from 'react'
-import { ModalContext } from '../../userAuth/singup/stepper/stepperContent/step2/career/Career';
+import { ModalContext } from '../../../context/ModalContext'
 
 interface Props {
     children: ReactNode
@@ -13,7 +13,6 @@ const ModalWrapper = ({ children }: Props) => {
             keepMounted
             open={context.open}
             closeAfterTransition
-            onClose={() => context.handleClose(false)}
             aria-labelledby="keep-mounted-modal-title"
             aria-describedby="keep-mounted-modal-description"
         >
