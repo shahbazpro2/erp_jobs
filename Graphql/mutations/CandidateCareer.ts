df9a2f79b6dee65389c76f4fd186aa57716b3e7b
shahbaz <shahbazrock0321@gmail.com>
2021-09-22 20:23:30 +0500

    Make CandidateCareer mutation

---
+++ Graphql/mutations/CandidateCareer.ts
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client'
+
+export const CandidateCareer = gql`
+    mutation createCareer(
+            $jobTitle:String!,
+            $companyName: String!,
+            $companyLocation:String!,
+            $fromDate:Date!,
+            $toDate:Date!,
+            $currentWorkHere:Boolean!,
+            $confidential:Boolean!
+            $description:String!,
+        
+    ){
+createCareer(
+    input:{
+            jobTitle:$jobTitle,
+            companyName:$companyName,
+            companyLocation:$companyLocation,
+            fromDate:$fromDate,
+            toDate:$toDate,
+            currentWorkHere:$currentWorkHere,
+            description:$description,
+            confidential:$confidential,
+        
+    }){
+        Career{
+        id
+    }}
+    }
+`
