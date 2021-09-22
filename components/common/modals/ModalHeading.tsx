013a2426648e56c036c28619fd038052afff3039
shahbaz <shahbazrock0321@gmail.com>
2021-09-22 18:58:45 +0500

    Use context and pass to onClick

--- components/common/modals/ModalHeading.tsx
+++ components/common/modals/ModalHeading.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { ModalContext } from '../../../context/ModalContext'
 interface Props {
     title: string
 }
 const ModalHeading = ({ title }: Props) => {
+    const context = useContext(ModalContext);
     return (
         <div className="flex">
             <div className="text-xl font-bold">
                 {title}
             </div>
-            <div className="close ml-auto cursor-pointer">
+            <div className="close ml-auto cursor-pointer" onClick={context.handleClose}>
                 <HighlightOffIcon />
             </div>
         </div>
