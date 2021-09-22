e79b7189eb94144edd225af23e25d7b9d4dc1ce8
shahbaz <shahbazrock0321@gmail.com>
2021-09-22 15:18:06 +0500

    Make BoxWrapper general

--- components/common/boxWrapper/BoxWrapper.tsx
+++ components/common/boxWrapper/BoxWrapper.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 
 interface Props {
-    title: String,
-    subtitle: String,
     children: React.ReactNode
 }
-const BoxWrapper = ({ children, title, subtitle }: Props) => {
+const BoxWrapper = ({ children }: Props) => {
     return (
         <div className="bg-white border-[1px] border-[#D9D9D9]  rounded-[7px] py-8 px-7">
-            <div className="text-2xl h-mid-clr">
-                {title}
-            </div>
-            <div className="text-base subtitle-clr tracking-[0px]">
-                {subtitle}
-            </div>
             {children}
         </div>
     )
