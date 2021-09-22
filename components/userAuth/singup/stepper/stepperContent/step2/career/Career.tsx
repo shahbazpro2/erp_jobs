c9395fef266a272dd56fa8190cc0c2ebec24ab7c
shahbaz <shahbazrock0321@gmail.com>
2021-09-22 17:52:28 +0500

    Use context to passing props to nested child

--- components/userAuth/singup/stepper/stepperContent/step2/career/Career.tsx
+++ components/userAuth/singup/stepper/stepperContent/step2/career/Career.tsx
@@ -1,11 +1,26 @@
-import React, { Dispatch, SetStateAction } from 'react'
-import AddItemsWrapper from '../../../../../common/addItemsWrapper/AddItemsWrapper';
+import React, { createContext, Dispatch, SetStateAction, useState } from 'react'
 import { Button, Divider } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import AddItemsWrapper from '../../../../../../common/addItemsWrapper/AddItemsWrapper';
+import CareerContent from './CareerContent';
 interface Props {
     setActive: Dispatch<SetStateAction<string>>
 }
+export const ModalContext = createContext({
+    open: false,
+    handleClose: (val: boolean) => { }
+})
+
 const Career = ({ setActive }: Props) => {
+    const [open, setOpen] = useState(false)
+
+    const ContextValue = {
+        open: open,
+        handleClose: (val: boolean) => {
+            setOpen(val)
+        }
+    }
+
     const onBack = () => {
         setActive('basic')
     }
@@ -18,7 +33,7 @@ const Career = ({ setActive }: Props) => {
             <div className="col-start-3 col-span-3">
                 <AddItemsWrapper title="Career Journey" subtitle="Add your career journey, you can add multiple careers." onBack={onBack} onContinue={onContinue} skip={false}>
                     <div className="mt-7">
-                        <Button variant="outlined" className="w-full" color="primary">
+                        <Button variant="outlined" className="w-full" color="primary" onClick={() => setOpen(true)}>
                             Add New <AddIcon style={{ width: 18 }} />
                         </Button>
                     </div>
@@ -26,7 +41,9 @@ const Career = ({ setActive }: Props) => {
                         <Divider />
                     </div>
                 </AddItemsWrapper>
-
+                <ModalContext.Provider value={ContextValue}>
+                    <CareerContent />
+                </ModalContext.Provider>
             </div>
         </div>
     )
