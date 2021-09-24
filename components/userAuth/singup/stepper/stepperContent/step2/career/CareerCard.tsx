b3890c1273902029804a2a1b3147dba3dd6d6ba3
shahbaz <shahbazrock0321@gmail.com>
2021-09-24 16:05:19 +0500

    Check if currentorkHere then show present else show toDate

--- components/userAuth/singup/stepper/stepperContent/step2/career/CareerCard.tsx
+++ components/userAuth/singup/stepper/stepperContent/step2/career/CareerCard.tsx
@@ -28,7 +28,7 @@ const CareerCard = ({ data }: Props) => {
                     <div className="col-span-3">
                         <div className="text-sm flex">
                             <div className="ml-auto">
-                                {data.fromDate} - {data.toDate}
+                                {data.fromDate} - {data.currentWorkHere ? 'Present' : data.toDate}
                                 {data.confidential && <div className="subtitle-clr">Confidential</div>}
                                 <div className="flex mt-3 justify-center text-xs subtitle-clr">
                                     <div className="cursor-pointer mr-3 flex items-center primary-clr-hover">
