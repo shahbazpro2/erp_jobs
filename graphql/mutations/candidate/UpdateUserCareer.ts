4e0346e33d9cbb9e3b8e1aa4abd0b4ed6b33d4b2
shahbaz <shahbazrock0321@gmail.com>
2021-09-24 19:41:53 +0500

    Make UpdateUserCareer mutation

---
+++ graphql/mutations/candidate/UpdateUserCareer.ts
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client'
+
+export const UpdateUserCareer = gql`
+    mutation updateCareer(
+            $id:ID!,
+            $jobTitle:ID!,
+            $companyName: String!,
+            $companyLocation:String!,
+            $fromDate:Date!,
+            $toDate:Date,
+            $currentWorkHere:Boolean,
+            $confidential:Boolean
+            $description:String!,
+        
+    ){
+updateCareer(
+    id:$id,
+    input:{
+            
+            jobTitle:{  id:$jobTitle},
+            companyName:$companyName,
+            companyLocation:$companyLocation,
+            fromDate:$fromDate,
+            toDate:$toDate,
+            currentWorkHere:$currentWorkHere,
+            description:$description,
+            confidential:$confidential,
+        
+    }){
+        Career{
+        id
+    }}
+    }
+`
