14fb9de176a09a109708abed1c3b33e289258da9
shahbaz <shahbazrock0321@gmail.com>
2021-09-24 19:39:18 +0500

    Make initialStates file for career

---
+++ components/userAuth/singup/stepper/stepperContent/step2/career/initialStates.ts
@@ -0,0 +1,22 @@
+export const initialCareerState = {
+    jobTitle: ' ',
+    companyName: '',
+    companyLocation: '',
+    fromDate: '',
+    toDate: '',
+    currentWorkHere: false,
+    confidential: false,
+    description: ''
+}
+
+export const initialCareerEditState = {
+    id: '',
+    jobTitle: { id: '', name: ' ' },
+    companyName: '',
+    companyLocation: '',
+    fromDate: '',
+    toDate: '',
+    currentWorkHere: false,
+    confidential: false,
+    description: ''
+}
