5d0d90a967baa8bf398ec77cd64d20e5cc603d69
shahbaz <shahbazrock0321@gmail.com>
2021-09-28 18:38:36 +0500

    Add inputs for Education

--- components/userAuth/singup/stepper/stepperContent/step2/education/EducationInputs.tsx
+++ components/userAuth/singup/stepper/stepperContent/step2/education/EducationInputs.tsx
@@ -1,21 +1,18 @@
-import SelectField from '@components/common/textFields/SelectField'
 import TextFieldSimple from '@components/common/textFields/TextFieldSimple'
-import { DropdownContext } from '@context/DropdownContext'
-import { Button, Checkbox, FormControlLabel, MenuItem, TextField } from '@mui/material'
-import React, { ChangeEvent, SyntheticEvent, useContext } from 'react'
-import { CareerProps } from './types'
+import { Button } from '@mui/material'
+import React, { ChangeEvent, SyntheticEvent } from 'react'
+import { EducationProps } from './types'
 
 
 interface Props {
     onSubmit: (e: SyntheticEvent) => Promise<void>,
-    setState: (data: CareerProps) => void,
-    state: CareerProps,
+    setState: (data: EducationProps) => void,
+    state: EducationProps,
     inputError: boolean,
     editId: string
 }
 
 const EducationInputs = ({ onSubmit, setState, inputError, state, editId }: Props) => {
-    const jobContext = useContext(DropdownContext)
 
     const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
         const { value, name } = e.target
@@ -30,65 +27,13 @@ const EducationInputs = ({ onSubmit, setState, inputError, state, editId }: Prop
     return (
         <form noValidate autoComplete="off" onSubmit={onSubmit}>
             <div className="grid gap-5">
-                <SelectField
-                    inputError={inputError}
-                    value={state.jobTitle}
-                    name="jobTitle"
-                    label="Job Title"
-                    onChange={onChangeInput}
-                >
-                    <MenuItem disabled value={" "}>
-                        Select job title
-                    </MenuItem>
-                    {jobContext?.jobTitles?.map((title: any) => <MenuItem key={title.id} value={title.id}>{title.name}</MenuItem>)}
-                </SelectField>
-                <TextFieldSimple inputError={inputError} value={state.companyName} name="companyName" label="Company Name" onChange={onChangeInput} />
-                <TextFieldSimple inputError={inputError} value={state.companyName} name="companyLocation" label="Company Location" onChange={onChangeInput} />
-
+                <TextFieldSimple inputError={inputError} value={state.degreeTitle} name="degreeTitle" label="Degree" onChange={onChangeInput} />
+                <TextFieldSimple inputError={inputError} value={state.university} name="university" label="University" onChange={onChangeInput} />
+                <TextFieldSimple inputError={inputError} value={state.field} name="field" label="Field" onChange={onChangeInput} />
                 <div className="grid grid-cols-2 gap-3">
-                    <TextField
-                        required
-                        error={inputError && !state.fromDate ? true : inputError && new Date(state.fromDate) >= new Date(state.toDate) ? true : false}
-                        helperText={inputError && !state.fromDate ? 'Please select from date' : inputError && new Date(state.fromDate) >= new Date(state.toDate) ? 'From date must be less then to date' : ''}
-                        type="date"
-                        name="fromDate"
-                        label="From Date"
-                        variant="outlined"
-                        className="w-full"
-                        value={state.fromDate}
-                        onChange={onChangeInput}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        required
-                        error={(inputError && !state.toDate && !state.currentWorkHere) ? true : (inputError && !state.currentWorkHere) && new Date(state.fromDate) >= new Date(state.toDate) ? true : false}
-                        helperText={(inputError && !state.toDate && !state.currentWorkHere) ? 'Please select to date' : (inputError && !state.currentWorkHere) && new Date(state.fromDate) >= new Date(state.toDate) ? 'From date must be less then to date' : ''}
-                        disabled={state.currentWorkHere}
-                        type="date"
-                        name="toDate"
-                        label="To Date"
-                        variant="outlined"
-                        className="w-full"
-                        value={state.toDate}
-                        onChange={onChangeInput}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
+                    <TextFieldSimple inputError={inputError} value={state.passYear} name="passYear" label="Passing Year" onChange={onChangeInput} />
+                    <TextFieldSimple inputError={inputError} value={state.grade} name="grade" label="Grade" onChange={onChangeInput} />
                 </div>
-                <div className="grid grid-cols-3 gap-3">
-                    <FormControlLabel
-                        control={<Checkbox onChange={onChangeInput} checked={state.currentWorkHere} name="currentWorkHere" color="primary" />}
-                        label="I Currently work here"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox onChange={onChangeInput} checked={state.confidential} name="confidential" color="primary" />}
-                        label="Confidential"
-                    />
-                </div>
-                <TextFieldSimple multiline={true} inputError={inputError} value={state.companyName} name="description" label="Description" onChange={onChangeInput} />
 
                 <Button type="submit" variant="contained" color="primary" disableElevation >
                     {editId ? 'Update' : 'Save'}
