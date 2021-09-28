231689c3561dc2079040f46710818c518292d68f
shahbaz <shahbazrock0321@gmail.com>
2021-09-28 16:10:53 +0500

    Add EducationModalContext context

--- context/ModalContext.ts
+++ context/ModalContext.ts
@@ -1,10 +1,19 @@
 import { initialCareerEditState } from "@components/userAuth/singup/stepper/stepperContent/step2/career/initialStates";
 import { CareerQueryProps } from "@components/userAuth/singup/stepper/stepperContent/step2/career/types";
+import { initialEducationEditState } from "@components/userAuth/singup/stepper/stepperContent/step2/education/initialStates";
+import { EducationQueryProps } from "@components/userAuth/singup/stepper/stepperContent/step2/education/types";
 import { createContext } from "react";
 
-export const ModalContext = createContext({
+export const CareerModalContext = createContext({
     editData: initialCareerEditState,
     open: false,
     handleClose: () => { },
     handleEdit: (data: CareerQueryProps) => { }
+})
+
+export const EducationModalContext = createContext({
+    editData: initialEducationEditState,
+    open: false,
+    handleClose: () => { },
+    handleEdit: (data: EducationQueryProps) => { }
 })
