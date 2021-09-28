9921c5f87e1ced7d0500a15199da0a95a7d3fc83
shahbaz <shahbazrock0321@gmail.com>
2021-09-28 16:08:17 +0500

    Initial state for Education component

---
+++ components/userAuth/singup/stepper/stepperContent/step2/education/initialStates.ts
@@ -0,0 +1,14 @@
+
+
+export const initialEducationState = {
+    degreeTitle: '',
+    university: '',
+    field: '',
+    passYear: '',
+    grade: '',
+}
+
+export const initialEducationEditState = {
+    id: '',
+    ...initialEducationState
+}
