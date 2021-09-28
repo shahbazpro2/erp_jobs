87e55493a78c96c2db47c95482220becc040e23e
shahbaz <shahbazrock0321@gmail.com>
2021-09-28 16:11:21 +0500

    Make CreateEducation query

---
+++ graphql/mutations/user/education/CreateEducation.ts
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client'
+
+export const CreateEducation = gql`
+    mutation createEducation(
+            $degreeTitle:String!,
+            $university: String!,
+            $field:String!,
+            $passYear:String!,
+            $grade:String!,
+        
+    ){
+        createEducation(
+    input:{
+            degreeTitle:$degreeTitle,
+            university:$companyName,
+            field:$field,
+            passYear:$passYear,
+            grade:$grade,
+        
+    }){
+        Education{
+        id
+    }}
+    }
+`
