d81f066d223dc4fa96108ddc23890e87f9cd3509
shahbaz <shahbazrock0321@gmail.com>
2021-09-28 16:06:55 +0500

    Make Education component

--- components/userAuth/singup/stepper/stepperContent/step2/education/Education.tsx
+++ components/userAuth/singup/stepper/stepperContent/step2/education/Education.tsx
@@ -1,25 +1,44 @@
-import React, { Dispatch, SetStateAction, useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { Dispatch, Fragment, SetStateAction, useEffect, useState } from 'react'
 import { Button, Divider } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import AddItemsWrapper from '@components/common/addItemsWrapper/AddItemsWrapper';
-import { ModalContext } from '@context/ModalContext';
-import EducationContent from './EducationContent';
-
+import CareerContent from './EducationContent';
+import { EducationModalContext } from '@context/ModalContext'
+import CareerCard from './EducationCard';
+import { useLazyQuery, useMutation } from '@apollo/client';
+import { EducationQueryProps } from './types';
+import objectIsEmpty from '@components/functions/objectIsEmpty';
+import { DeleteCareer } from '@graphql/mutations/user/career/DeleteCareer';
+import { AllCareers } from '@graphql/queries/user/career/AllCareers';
+import DialogAlert from '@components/common/alerts/DialogAlert';
+import FeedbackApi from '@components/common/feedback/FeedbackAPi';
+import { initialEducationEditState } from './initialStates';
 interface Props {
     setActive: Dispatch<SetStateAction<string>>
 }
+
+
 const Education = ({ setActive }: Props) => {
+    const [deleteCareer] = useMutation(DeleteCareer, { refetchQueries: [{ query: AllCareers }] })
+    const [allCareers, { data }] = useLazyQuery(AllCareers)
     const [open, setOpen] = useState(false)
-    const [submit, setSubmit] = useState(false)
+    /*  const [data, setData] = useState<CareerQueryProps[]>([]) */
+    const [editData, setEditData] = useState<EducationQueryProps>(initialEducationEditState)
+    const [apiError, setApiError] = useState<string[]>([])
+    const [apiSuccess, setApiSuccess] = useState<string[]>([])
+    const [delId, setDelId] = useState(-1)
 
     const ContextValue = {
-        open: open,
-        submit: submit,
+        editData,
+        open,
         handleClose: () => {
+            setEditData(initialEducationEditState)
             setOpen(false)
         },
-        handleSubmit: () => {
-            setSubmit(true)
+        handleEdit: (data: EducationQueryProps) => {
+            setEditData(data)
+            setOpen(true)
         }
     }
 
@@ -27,31 +46,56 @@ const Education = ({ setActive }: Props) => {
         setActive('career')
     }
 
-    const onSkip = () => {
-        setActive('skills')
+    const onContinue = () => {
+        console.log('data', data)
+        data?.allCareers?.length ?
+            setActive('education') :
+            setApiError([`You must need to add atleast one career to continue`])
+
     }
 
-    const onContinue = () => {
-        setActive('skills')
+    const onDelete = async () => {
+        const id = delId
+        setDelId(-1)
+        try {
+            const res = await deleteCareer({ variables: { id } })
+            if (!objectIsEmpty(res)) {
+                setApiSuccess([`Career deleted successfully`])
+            }
+        } catch (err: any) {
+            setApiError(err.message)
+        }
     }
+
+    useEffect(() => {
+        allCareers()
+    }, [])
+
+
 
     return (
         <div className="grid grid-cols-7 justify-center">
             <div className="col-start-3 col-span-3">
-                <AddItemsWrapper title="Education" subtitle="Add your education, you can add multiple educations" onBack={onBack} onSkip={onSkip} onContinue={onContinue}>
-                    <div className="mt-7">
+                <EducationModalContext.Provider value={ContextValue}>
+                    <AddItemsWrapper title="Education" subtitle="Add your education, you can add multiple educations" onBack={onBack} onContinue={onContinue} skip={false}>
+                        <div className="mt-7 grid gap-2">
+                            {data?.allCareers?.map((career: EducationQueryProps, index: string) => <Fragment key={index}><CareerCard data={career} onDelete={setDelId} /></Fragment>)}
 
-                        <Button variant="outlined" className="w-full" color="primary" onClick={() => setOpen(true)}>
-                            Add New <AddIcon style={{ width: 18 }} />
-                        </Button>
-                    </div>
-                    <div className="my-7">
-                        <Divider />
-                    </div>
-                </AddItemsWrapper>
-                <ModalContext.Provider value={ContextValue}>
-                    <EducationContent />
-                </ModalContext.Provider>
+                        </div>
+                        <div className="mt-7">
+                            <Button variant="outlined" className="w-full" color="primary" onClick={() => setOpen(true)}>
+                                Add New <AddIcon style={{ width: 18 }} />
+                            </Button>
+                        </div>
+                        <div className="my-7">
+                            <Divider />
+                        </div>
+                    </AddItemsWrapper>
+
+                    <CareerContent />
+                </EducationModalContext.Provider>
+                <DialogAlert open={delId < 0 ? false : true} title="Are to sure to delete career?" handleClose={() => setDelId(-1)} handleAccept={onDelete} />
+                <FeedbackApi apiError={apiError} apiSuccess={apiSuccess} setApiError={setApiError} setApiSuccess={setApiSuccess} />
             </div>
         </div>
     )
