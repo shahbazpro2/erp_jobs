b4cce8ac7b8eaf4ddab0d17431a619496926d1b7
shahbaz <shahbazrock0321@gmail.com>
2021-09-29 18:01:18 +0500

    Wrap with ApolloWrapper and GetUserWrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,10 @@ import { NoSsr, ThemeProvider } from '@mui/material'
 import theme from '../theme'
 import { Provider } from 'react-redux'
 import { store } from '../redux/Store'
-import IsUserWrapper from '../components/IsUserWrapper'
+import IsUserWrapper from '../components/wrappers/RouteWrapper'
+import GetUserWrapper from '@components/wrappers/GetUserWrapper'
+import RouteWrapper from '../components/wrappers/RouteWrapper'
+import ApolloWrapper from '@components/wrappers/ApolloWrapper'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,9 +17,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
     <ThemeProvider theme={theme}>
       <NoSsr>
-        <IsUserWrapper>
-          <Component {...pageProps} />
-        </IsUserWrapper>
+        <GetUserWrapper>
+          <ApolloWrapper>
+            <Component {...pageProps} />
+          </ApolloWrapper>
+        </GetUserWrapper>
       </NoSsr>
     </ThemeProvider>
   </Provider>
