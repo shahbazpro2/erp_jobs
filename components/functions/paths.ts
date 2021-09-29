02bfb515067e0c1f76940611ad4eea124ea728e4
shahbaz <shahbazrock0321@gmail.com>
2021-09-29 18:02:02 +0500

    Refactor path file

--- components/functions/paths.ts
+++ components/functions/paths.ts
@@ -1,37 +1,13 @@
 interface Props {
     pathname: string
 }
-export const isProtectedRoute = ({ pathname }: Props) => {
-    if (
-        [
-            "/test",
-            "",
-        ].includes(pathname)
-    )
-        return true;
-    return false;
-};
-export const isLoggedInRoute = ({ pathname }: Props) => {
-    if (
-        [
-            "/login/user",
-        ].includes(pathname)
-    )
-        return true;
-    return false;
-};
-/* export const isOnlyGuestRoute = ({pathname}:Props) => {
-    if (pathname === "/" || pathname === "/login" || pathname === "/reset") return true;
-    return false;
-}; */
 
-export const isPublicRoute = ({ pathname }: Props) => {
-    if (
-        [
-            "/register/user",
-            "/login/user",
-        ].includes(pathname)
-    )
-        return true;
-    return false;
+export const isProtectedRoute = ({ pathname }: Props) => {
+    return ["/test", "",].includes(pathname)
+}
+
+// Determines whether the route is a login route
+export const isGuestRoute = ({ pathname }: Props) => {
+    return ["/login/user",].includes(pathname)
 };
+
