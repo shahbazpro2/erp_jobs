6ad0de039761b48e70565a740e6eef9b6f3253b3
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 20:08:42 +0500

    Make News component

---
+++ pages/home/news/News.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const News = () => {
+    return (
+        <div className="bg-[#F4F7FA]">
+            <div className="grid grid-cols-12">
+                <div className="col-start-3 col-span-8">
+                    <div className="py-32">
+
+                        <div className="text-center">
+                            <div className="text-4xl font-bold">
+                                News that helps
+                            </div>
+                            <div className="text-xl m-auto opacity-[0.7] mt-5">
+                                Read many articles about jobs and how to get them easily .
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default News
