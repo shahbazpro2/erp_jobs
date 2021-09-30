c73c596e6ff44e03e206a320e54ade787cf85f85
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 20:35:24 +0500

    Make Subscription component

---
+++ pages/home/subscription/Subscription.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Subscription = () => {
+    return (
+        <div className="bg-white py-16">
+            <div className="grid grid-cols-12">
+                <div className="col-start-3 col-span-8">
+                    <div className="grid grid-cols-2">
+                        <div className="text-3xl">
+                            Subscribe to our newsletter to get latest jobs on your inbox.
+                        </div>
+                        <div className="subscribe">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Subscription
