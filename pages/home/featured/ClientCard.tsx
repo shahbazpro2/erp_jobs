98ce45b24afea0da6964c9631eb8d66c2e83f894
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 16:40:49 +0500

    Add image

--- pages/home/featured/ClientCard.tsx
+++ pages/home/featured/ClientCard.tsx
@@ -1,5 +1,6 @@
 import { Card } from '@mui/material'
 import React from 'react'
+import Image from 'next/image'
 
 
 interface Props {
@@ -15,8 +16,9 @@ const ClientCard = ({ title, icon }: Props) => {
                 boxShadow: '0px 34px 33px #161C2D21'
             },
         }}>
-            <div className="flex items-center h-[5rem] px-3">
-                <div className="text-xl font-bold">
+            <div className="flex items-center h-[6rem] px-3">
+                <Image src={icon} alt="icon" width="70%" height="70%" />
+                <div className="text-xl font-bold ml-3">
                     {title}
                 </div>
             </div>
