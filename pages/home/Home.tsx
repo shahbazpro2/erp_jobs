3958c646e7e772ee1af0c260f0092ed986b3fec1
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 13:09:32 +0500

    Make Home compoent and use TopSection component

---
+++ pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import UserNavbar from '@components/common/userNavbar/UserNavbar'
+import React from 'react'
+import TopSection from './topSection/TopSection'
+
+const Home = () => {
+    return (
+        <div>
+            <TopSection />
+
+        </div>
+    )
+}
+
+export default Home
