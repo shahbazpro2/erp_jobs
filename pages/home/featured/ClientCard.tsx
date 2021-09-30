9315ce059943a0366afeb5b868c3eee422796882
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 16:18:04 +0500

    Dynamic title and icon and boxShadow on hover

--- pages/home/featured/ClientCard.tsx
+++ pages/home/featured/ClientCard.tsx
@@ -1,12 +1,23 @@
 import { Card } from '@mui/material'
 import React from 'react'
 
-const ClientCard = () => {
+
+interface Props {
+    title: string,
+    icon: string
+}
+
+const ClientCard = ({ title, icon }: Props) => {
     return (
-        <Card variant="outlined" sx={{ borderRadius: '10px' }}>
+        <Card variant="outlined" sx={{
+            borderRadius: '10px', "&:hover": {
+                cursor: 'pointer',
+                boxShadow: '0px 34px 33px #161C2D21'
+            },
+        }}>
             <div className="flex items-center h-[5rem] px-3">
                 <div className="text-xl font-bold">
-                    Tokopedia
+                    {title}
                 </div>
             </div>
         </Card>
