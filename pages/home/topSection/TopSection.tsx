ae9ec94bf198957e640d7bb4a885814ff104484d
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 13:09:05 +0500

    Make TopSection component

---
+++ pages/home/topSection/TopSection.tsx
@@ -0,0 +1,12 @@
+import UserNavbar from '@components/common/userNavbar/UserNavbar'
+import React from 'react'
+
+const TopSection = () => {
+    return (
+        <div className="bg-[#473BF017] h-screen">
+            <UserNavbar />
+        </div>
+    )
+}
+
+export default TopSection
