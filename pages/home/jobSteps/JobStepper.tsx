ef1f0170e8706e6aa37f6f3888d7372f89aa51d2
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 19:12:04 +0500

    JobStepper dynamic data

--- pages/home/jobSteps/JobStepper.tsx
+++ pages/home/jobSteps/JobStepper.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
 
-const JobStepper = () => {
+
+interface Props {
+    step: number,
+    title: string,
+    content: string
+}
+
+const JobStepper = ({ step, title, content }: Props) => {
     return (
         <div className="grid grid-flow-col grid-cols-6">
             <div className="col-span-1">
                 <div className="rounded-full m-auto primary-bg  w-10 h-10  flex items-center justify-center text-white mt-1">
-                    1
+                    {step}
                 </div>
 
             </div>
             <div className="content col-span-3">
                 <div className="text-2xl">
-                    Search for a job
+                    {title}
                 </div>
                 <div className="mt-3">
                     <div className="text-base opacity-[0.7]">
-                        With lots of unique blocks, you can easily build a page without coding.
+                        {content}
                     </div>
                 </div>
             </div>
