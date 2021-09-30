f99f1054bd64e48a24baa3a90446820c332f3762
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 16:06:05 +0500

    Import Featured component

--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -1,12 +1,13 @@
 import UserNavbar from '@components/common/userNavbar/UserNavbar'
 import React from 'react'
+import Featured from './featured/Featured'
 import TopSection from './topSection/TopSection'
 
 const Home = () => {
     return (
         <div>
             <TopSection />
-
+            <Featured />
         </div>
     )
 }
