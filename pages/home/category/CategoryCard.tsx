fca920b660fdf0e2ea5c6cb32a1a3aa5e8bec2f5
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 17:15:20 +0500

    Make CategoryCard component

---
+++ pages/home/category/CategoryCard.tsx
@@ -0,0 +1,40 @@
+import { Card } from '@mui/material'
+import React from 'react'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+
+interface Props {
+    title: string,
+    count: string
+}
+
+const CategoryCard = ({ title, count }: Props) => {
+    return (
+        <div className="category-card-hover">
+            <Card variant="outlined" sx={{
+                background: 'white',
+                borderRadius: '10px', "&:hover": {
+                    cursor: 'pointer',
+                    backgroundColor: '#473BF0',
+                    color: 'white'
+                },
+            }}>
+                <div className="flex items-center h-[7rem]">
+                    <div className='ml-7'>
+                        <div className="text-xl font-bold">
+                            {title}
+                        </div>
+                        <div className="text-base mt-1">
+                            {`${count} Jobs`}
+                        </div>
+                    </div>
+                    <div id="category-arrow" className="rounded-full w-11 h-11 ml-auto mr-5 flex items-center justify-center bg-white">
+                        <ArrowForwardIcon sx={{ color: 'black' }} />
+                    </div>
+                </div>
+            </Card>
+        </div>
+    )
+}
+
+export default CategoryCard
