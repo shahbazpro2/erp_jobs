3981dcb6909a893a7b72c6df2536e067efe3d9c6
shahbaz <shahbazrock0321@gmail.com>
2021-09-30 16:41:07 +0500

    Import ClientCard

--- pages/home/featured/Featured.tsx
+++ pages/home/featured/Featured.tsx
@@ -6,7 +6,7 @@ const Featured = () => {
         <div className="bg-white">
             <div className="grid grid-cols-12">
                 <div className="col-start-3 col-span-8">
-                    <div className="flex items-center h-[40rem]">
+                    <div className="flex items-center h-[43rem]">
                         <div className="w-full">
                             <div className="text-4xl font-bold">
                                 Featured Clients
@@ -14,16 +14,16 @@ const Featured = () => {
                             <div className="text-xl w-[18rem] opacity-[0.7] mt-3">
                                 Check out our featured clients and browse their latest jobs.
                             </div>
-                            <div className="mt-14">
+                            <div className="mt-20">
                                 <div className="grid grid-rows-2 grid-cols-4 gap-10 px-3">
-                                    <ClientCard title="Tokopedia" icon="" />
-                                    <ClientCard title="Dropbox" icon="" />
-                                    <ClientCard title="Evernote" icon="" />
-                                    <ClientCard title="Mailchimp" icon="" />
-                                    <ClientCard title="Airbnb" icon="" />
-                                    <ClientCard title="Airbnb" icon="" />
-                                    <ClientCard title="Twitch" icon="" />
-                                    <ClientCard title="Amazon" icon="" />
+                                    <ClientCard title="Tokopedia" icon="/assets/images/tokopedia.png" />
+                                    <ClientCard title="Dropbox" icon="/assets/images/dropbox.png" />
+                                    <ClientCard title="Evernote" icon="/assets/images/evernote.svg" />
+                                    <ClientCard title="Mailchimp" icon="/assets/images/mailchimp.png" />
+                                    <ClientCard title="Airbnb" icon="/assets/images/airbnb.svg" />
+                                    <ClientCard title="Airbnb" icon="/assets/images/football.svg" />
+                                    <ClientCard title="Twitch" icon="/assets/images/twitch.svg" />
+                                    <ClientCard title="Amazon" icon="/assets/images/amazon.svg" />
                                 </div>
                             </div>
                         </div>
