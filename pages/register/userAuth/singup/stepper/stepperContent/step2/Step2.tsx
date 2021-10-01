e6955c237032b82b9b4bee5274fb93c71f23a38b
shahbaz <shahbazrock0321@gmail.com>
2021-10-01 18:42:55 +0500

    Update skills path

--- pages/register/userAuth/singup/stepper/stepperContent/step2/Step2.tsx
+++ pages/register/userAuth/singup/stepper/stepperContent/step2/Step2.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import BasicInformation from './basic/BasicInfo'
 import CareerWrapper from './CareerWrapper'
 import EducationWrapper from './EducationWrapper'
-import Skills from './skills/Skills'
+import Skills from './Skills'
 
 const Step2 = () => {
     const [active, setActive] = useState('basic')
