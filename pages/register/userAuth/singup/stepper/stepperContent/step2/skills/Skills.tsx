1f8927f01161c246872e362d998a03649e4e2b37
shahbaz <shahbazrock0321@gmail.com>
2021-10-01 18:40:57 +0500

    Fix autocomplete with dynamic data

--- pages/register/userAuth/singup/stepper/stepperContent/step2/skills/Skills.tsx
+++ pages/register/userAuth/singup/stepper/stepperContent/step2/skills/Skills.tsx
@@ -4,74 +4,78 @@ import { Autocomplete, Chip, Divider, TextField } from '@mui/material';
 import AddItemsWrapper from '@components/common/addItemsWrapper/AddItemsWrapper';
 import { useLazyQuery, useMutation } from '@apollo/client';
 import FeedbackApi from '@components/common/feedback/FeedbackAPi';
-import { AllEducations } from '@graphql/queries/user/education/AllEducations';
-import SkillsContent from './SkillsContent';
+import { AllSkills } from '@graphql/queries/common/AllSkills';
+import { CreateSkills } from '@graphql/mutations/user/skills/CreateSkills';
+import { CandidateSkills } from '@graphql/queries/user/skills/CandidateSkills';
+import graphqlRes from '@components/functions/graphqlRes';
 
 interface Props {
     setActive: Dispatch<SetStateAction<string>>
 }
 
+type AllSkill = {
+    id: string,
+    name: string
+}
 
 const Skills = ({ setActive }: Props) => {
-    const [allEducations, { data }] = useLazyQuery(AllEducations, { fetchPolicy: 'cache-and-network' })
-    const fixedOptions = [{ id: 0, name: 'reactjs' }, { id: 1, name: 'angular' }, { id: 2, name: 'nodejs' }];
+    const [allEducations, { data }] = useLazyQuery(AllSkills)
+    const [createSkills] = useMutation(CreateSkills, { refetchQueries: [CandidateSkills], onError: () => null })
+    const [allSkills, setAllSkills] = useState<AllSkill[]>([])
     const [value, setValue] = React.useState<any>([]);
     const [apiError, setApiError] = useState<string[]>([])
     const [apiSuccess, setApiSuccess] = useState<string[]>([])
 
 
+    useEffect(() => {
+        console.log(data)
+        if (data?.allSkills)
+            setAllSkills(data?.allSkills)
+    }, [data])
 
-
-    console.log('data', data)
 
     const onBack = () => {
-        setActive('career')
+        setActive('education')
     }
 
-    const onContinue = () => {
-        setActive('skills')
+    const onContinue = async () => {
+        /* const { error, data } = await graphqlRes(createSkills({ variables: { id: Number(value.id), name: value.name } }))
+
+        if (error) {
+            setApiError(data)
+            return
+        }
+        console.log('success', data) */
+        //setApiSuccess(data)
+
+
+        setActive('certificates')
     }
 
     useEffect(() => {
         allEducations()
     }, [])
 
-
+    console.log(value.length)
 
     return (
         <div className="grid grid-cols-7 justify-center">
             <div className="col-start-3 col-span-3">
-                <AddItemsWrapper title="Skills" subtitle="Add your skills, you can add multiple skills" disabled={true} onBack={onBack} onContinue={onContinue} skip={true}>
+                <AddItemsWrapper title="Skills" subtitle="Add your skills, you can add multiple skills" disabled={!value.length} onBack={onBack} onContinue={onContinue} skip={true}>
                     <div className="mt-7 grid gap-2">
                         <Autocomplete
+                            disableClearable
                             multiple
-                            id="fixed-tags-demo"
-                            value={value}
-                            onChange={(event, newValue) => {
-                                setValue([
-                                    ...fixedOptions,
-                                    ...newValue.filter((option) => fixedOptions.indexOf(option) === -1),
-                                ]);
-                            }}
-                            options={fixedOptions.map(op => op.name)}
-                            renderTags={(tagValue, getTagProps) => {
-                                console.log('tag', tagValue, getTagProps)
-                                return tagValue.map((option, index) => (
-                                    <Fragment key={index}>
-
-                                        <Chip
-                                            label={option.name}
-
-                                        />
-                                    </Fragment>
-                                ))
-                            }
-                            }
+                            id="multiple-limit-tags"
+                            onChange={(event, newValue) => setValue(newValue)}
+                            options={allSkills}
+                            getOptionLabel={(option) => option.name}
                             renderInput={(params) => (
-                                <TextField {...params} label="Add skills" placeholder="Favorites" InputLabelProps={{
-                                    shrink: true,
+                                <TextField variant="outlined"  {...params} placeholder="Skills" InputLabelProps={{
+                                    shrink: false,
                                 }} />
                             )}
+
                         />
                     </div>
                     <div className="my-7">
