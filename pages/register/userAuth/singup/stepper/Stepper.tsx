f99f606cfe88afd557ecf69d37817fe6c67baa82
shahbaz <shahbazrock0321@gmail.com>
2021-10-01 17:59:56 +0500

    Update path

--- pages/register/userAuth/singup/stepper/Stepper.tsx
+++ pages/register/userAuth/singup/stepper/Stepper.tsx
@@ -7,7 +7,7 @@ import Spinner from '@components/common/spinner/Spinner';
 import HorizontalStepper from '@components/common/stepper/HorizontalStepper'
 import Step1 from './stepperContent/step1/Step1';
 import Step2 from './stepperContent/step2/Step2';
-import { AllJobtitles } from '@graphql/queries/AllJobTitles';
+import { AllJobtitles } from '@graphql/queries/common/AllJobTitles';
 
 const steps = ["Create an account", "Build your profile", "Upload your CV"];
 const Stepper = () => {
