012267681605f17cb6b88699f82bfefba4725899
shahbaz <shahbazrock0321@gmail.com>
2021-10-01 14:39:17 +0500

    Make SubscriptionForm component

---
+++ pages/home/subscription/SubscriptionForm.tsx
@@ -0,0 +1,29 @@
+import { Button, TextField } from '@mui/material'
+import React from 'react'
+
+const SubscriptionForm = () => {
+    return (
+        <div>
+            <form>
+
+                <div className="grid grid-cols-7 gap-5 ">
+                    <div className="col-span-5">
+                        <TextField
+                            variant="outlined"
+                            placeholder="Enter your email"
+                            fullWidth
+                        />
+                    </div>
+                    <div className="col-span-2">
+
+                        <Button variant="contained" color="primary" className="h-full" disableElevation>
+                            Subscribe
+                        </Button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SubscriptionForm
