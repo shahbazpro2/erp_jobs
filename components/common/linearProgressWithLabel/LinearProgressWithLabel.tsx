d013735f950525bccf0fe5977971b25852ad4917
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 16:19:18 +0500

    Make LinearProgressWithLabel component

---
+++ components/common/linearProgressWithLabel/LinearProgressWithLabel.tsx
@@ -0,0 +1,19 @@
+import { LinearProgress, LinearProgressProps, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress sx={{ height: '7px', borderRadius: '7px' }} variant="determinate" {...props} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${Math.round(
+                    props.value,
+                )}%`}</Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default LinearProgressWithLabel
