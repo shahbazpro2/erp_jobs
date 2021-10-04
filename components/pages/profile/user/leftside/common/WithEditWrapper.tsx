54ba94a82724bcab8e5c2c06460ce61e4dfa23e5
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 19:01:11 +0500

    Make WithEditWrapper component

---
+++ components/pages/profile/user/leftside/common/WithEditWrapper.tsx
@@ -0,0 +1,31 @@
+import BoxWrapper from '@components/common/boxWrapper/BoxWrapper'
+import React, { ReactNode } from 'react'
+import EditIcon from '@mui/icons-material/Edit';
+
+
+interface Props {
+    children: ReactNode,
+    title: string,
+    onEditHandle: () => void
+}
+
+const WithEditWrapper = ({ children, title, onEditHandle }: Props) => {
+    return (
+        <BoxWrapper>
+            <div className="flex justify-between">
+                <div className="text-lg">
+                    {title}
+                </div>
+                <div className="flex cursor-pointer primary-clr-hover" onClick={onEditHandle}>
+                    <EditIcon sx={{ width: '16px' }} />
+                    <div className="ml-1">Edit</div>
+                </div>
+            </div>
+            <div className="mt-5">
+                {children}
+            </div>
+        </BoxWrapper>
+    )
+}
+
+export default WithEditWrapper
