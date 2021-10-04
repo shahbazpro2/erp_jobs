f3ff6d4ee8597b9fb5f774bf0dcabd7a5faa2c7e
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 19:11:49 +0500

    Import GridFlow component

--- components/pages/profile/user/leftside/personalInfo/PersonalInfo.tsx
+++ components/pages/profile/user/leftside/personalInfo/PersonalInfo.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import WithEditWrapper from '../common/WithEditWrapper';
+import GridFlow from './GridFlow';
 
 
 
 const PersonalInfo = () => {
     return (
         <WithEditWrapper title="Personal Information" onEditHandle={() => console.log('edit')}>
-
+            <div className="grid grid-cols-2 gap-4">
+                <GridFlow title="Birth Date" value="10 May 1997" />
+            </div>
         </WithEditWrapper>
     )
 }
