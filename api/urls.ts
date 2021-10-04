f584be418b06db05a1e3154f50e3a07a10280c79
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 20:24:48 +0500

    Add getCertificates url

--- api/urls.ts
+++ api/urls.ts
@@ -2,3 +2,4 @@ const hostUrl = 'http://10.104.45.78:8000'
 export const registerUserUrl = `${hostUrl}/register/candidate/`
 export const loginUserUrl = `${hostUrl}/login/candidate/`
 export const getUserUrl = `${hostUrl}/users/me/`
+export const getCertificates = `${hostUrl}/candidate/certificates/`
