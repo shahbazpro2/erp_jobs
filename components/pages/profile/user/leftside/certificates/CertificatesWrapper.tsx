449a11af16236eda1596c815834141ab5eeb0975
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 20:25:28 +0500

    Make CertificatesWrapper component

---
+++ components/pages/profile/user/leftside/certificates/CertificatesWrapper.tsx
@@ -0,0 +1,28 @@
+import { getUserCertificates } from '@api/Certificates'
+import { useLazyQuery } from '@apollo/client'
+import React, { useEffect, useState } from 'react'
+import SimpleWrapper from '../common/SimpleWrapper'
+import Certificates from './Certificates'
+
+const CertificatesWrapper = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+
+        //allCareers()
+        (async () => {
+            const res = await getUserCertificates()
+            if (!res?.error) {
+                setData(res?.data)
+            }
+        })()
+    }, [])
+
+    return (
+        <SimpleWrapper title="Certificates">
+            <Certificates data={data} />
+        </SimpleWrapper>
+    )
+}
+
+export default CertificatesWrapper
