0f78a5bbfac20f625a7b990021bb2e9647e78778
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 19:48:16 +0500

    Change Path

--- context/ModalContext.ts
+++ context/ModalContext.ts
@@ -1,9 +1,9 @@
 import { initialCertificateEditState } from "@components/common/certificates/initialStates";
 import { CertificateQueryProps } from "@components/common/certificates/types";
-import { initialEducationEditState } from "@components/common/education/initialStates";
-import { EducationQueryProps } from "@components/common/education/types";
 import { initialCareerEditState } from "@components/pages/profile/user/leftside/career/initialStates";
 import { CareerQueryProps } from "@components/pages/profile/user/leftside/career/types";
+import { initialEducationEditState } from "@components/pages/profile/user/leftside/education/initialStates";
+import { EducationQueryProps } from "@components/pages/profile/user/leftside/education/types";
 import { createContext } from "react";
 
 export const CareerModalContext = createContext({
