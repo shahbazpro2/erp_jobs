98405ea741efdd4d7c8e5eba7cf2fabbf6fa6ced
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 13:23:06 +0500

    Wrap with HeaderFooter and use ngProgress on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,28 +2,47 @@ import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import { NoSsr, ThemeProvider } from '@mui/material'
+import NProgress from 'nprogress'
 import theme from '../theme'
 import { Provider } from 'react-redux'
 import { store } from '../redux/Store'
-import IsUserWrapper from '../components/wrappers/RouteWrapper'
+import Head from 'next/head'
 import GetUserWrapper from '@components/wrappers/GetUserWrapper'
-import RouteWrapper from '../components/wrappers/RouteWrapper'
 import ApolloWrapper from '@components/wrappers/ApolloWrapper'
+import HeaderFooterWrapper from '@components/wrappers/HeaderFooterWrapper'
+import Router from 'next/router'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // NProgress configuration and setup
+  NProgress.configure({ easing: 'ease', showSpinner: false });
+
+  Router.events.on('routeChangeStart', () => NProgress.start())
+  Router.events.on('routeChangeComplete', () => NProgress.done())
+  Router.events.on('routeChangeError', () => NProgress.done())
 
 
-  return <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <NoSsr>
-        <GetUserWrapper>
-          <ApolloWrapper>
-            <Component {...pageProps} />
-          </ApolloWrapper>
-        </GetUserWrapper>
-      </NoSsr>
-    </ThemeProvider>
-  </Provider>
+  return <>
+    <Head>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/nprogress@0.2.0/nprogress.css"
+      />
+    </Head>
+
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <NoSsr>
+          <GetUserWrapper>
+            <ApolloWrapper>
+              <HeaderFooterWrapper>
+                <Component {...pageProps} />
+              </HeaderFooterWrapper>
+            </ApolloWrapper>
+          </GetUserWrapper>
+        </NoSsr>
+      </ThemeProvider>
+    </Provider>
+  </>
 }
 export default MyApp
