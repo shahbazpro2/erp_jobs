98cf411d986a664f5111aac40452f0650d556d60
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 19:22:27 +0500

    Add dummy data

--- components/pages/profile/user/leftside/personalInfo/PersonalInfo.tsx
+++ components/pages/profile/user/leftside/personalInfo/PersonalInfo.tsx
@@ -9,6 +9,15 @@ const PersonalInfo = () => {
         <WithEditWrapper title="Personal Information" onEditHandle={() => console.log('edit')}>
             <div className="grid grid-cols-2 gap-4">
                 <GridFlow title="Birth Date" value="10 May 1997" />
+                <GridFlow title="Gender" value="Male" />
+                <GridFlow title="Experience" value="1 Years" />
+                <GridFlow title="Country" value="Pakistan" />
+                <GridFlow title="Minimum Salary" value="2000 Euro" />
+                <GridFlow title="Profile Visibility" value="Registered only" />
+                <GridFlow title="City" value="Rawalpindi" />
+                <GridFlow title="Mobile" value="+923365121663" />
+                <GridFlow title="Email" value="shahbazrock0321@gmail.com" />
+                <GridFlow title="Address" value="Street#15b House#12 Bahar Colony near Lakhan Chowk Chakri Road" />
             </div>
         </WithEditWrapper>
     )
