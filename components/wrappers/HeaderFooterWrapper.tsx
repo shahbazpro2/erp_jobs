21ee72310f2f5d680fe62dc99b5590d271ebe670
shahbaz <shahbazrock0321@gmail.com>
2021-10-04 13:22:27 +0500

    Make HeaderFooter wrapper

---
+++ components/wrappers/HeaderFooterWrapper.tsx
@@ -0,0 +1,36 @@
+import Footer from '@components/common/footer/Footer'
+import Header from '@components/common/header/Header'
+import { HeaderContext } from '@context/HeaderContext'
+import React, { ReactNode, useState } from 'react'
+
+
+interface Props {
+    children: ReactNode
+}
+
+const HeaderFooterWrapper = ({ children }: Props) => {
+    const [state, setState] = useState({
+        bg: 'bg-white',
+        boxShadow: 'boxShadow'
+    })
+
+    const ContextValue = {
+        bg: state.bg,
+        boxShadow: state.boxShadow,
+        handleHeader: (bg: string, boxShadow: string) => {
+            setState({ bg, boxShadow })
+        }
+
+    }
+
+
+    return (
+        <HeaderContext.Provider value={ContextValue}>
+            <Header bg={state.bg} boxShadow={state.boxShadow} />
+            {children}
+            <Footer />
+        </HeaderContext.Provider>
+    )
+}
+
+export default HeaderFooterWrapper
