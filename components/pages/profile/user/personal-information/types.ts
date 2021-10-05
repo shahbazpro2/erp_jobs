0d39fcba8180b5c3f380e1a5f8007c4d943ba973
shahbaz <shahbazrock0321@gmail.com>
2021-10-05 15:44:44 +0500

    Remove nationality

--- components/pages/profile/user/personal-information/types.ts
+++ components/pages/profile/user/personal-information/types.ts
@@ -3,7 +3,6 @@ export interface BasicInfoProps {
     jobTitle: string,
     dateOfBirth: string,
     gender: string,
-    nationality: string,
     residenceCountry: string,
     city: string,
     phone: string,
