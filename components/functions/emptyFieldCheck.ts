ab8f7769c365671eaf2595b3d06cfd9b52144baa
shahbaz <shahbazrock0321@gmail.com>
2021-10-05 15:01:57 +0500

    Fix null value not check

--- components/functions/emptyFieldCheck.ts
+++ components/functions/emptyFieldCheck.ts
@@ -1,7 +1,8 @@
 const EmptyFieldCheck = (data: any) => {
     let empty = false
     for (const key in data) {
-        if (data[key] && data[key].toString().replace(/ /g, '') === '') {
+
+        if (data[key].toString().replace(/ /g, '') === "") {
             empty = true;
             break
         }
