c3e7a8c80e75627c0b2fbd6f6fa21aed71f2b2a6
shahbaz <shahbazrock0321@gmail.com>
2021-10-05 17:07:56 +0500

    Replace static data with server data

--- components/pages/profile/user/profile/leftside/personalInfo/PersonalInfo.tsx
+++ components/pages/profile/user/profile/leftside/personalInfo/PersonalInfo.tsx
@@ -1,3 +1,5 @@
+import capitalizeFirstLetter from '@components/functions/capitalizeFirstLetter';
+import moment from 'moment';
 import { useRouter } from 'next/router';
 import React from 'react'
 import WithEditWrapper from '../common/WithEditWrapper';
@@ -5,21 +7,21 @@ import GridFlow from './GridFlow';
 
 
 
-const PersonalInfo = () => {
+const PersonalInfo = ({ data }: any) => {
     const router = useRouter()
     return (
         <WithEditWrapper title="Personal Information" onEditHandle={() => router.push('/profile/user/personal-information')}>
             <div className="grid grid-cols-2 gap-4">
-                <GridFlow title="Birth Date" value="10 May 1997" />
-                <GridFlow title="Gender" value="Male" />
-                <GridFlow title="Experience" value="1 Years" />
-                <GridFlow title="Country" value="Pakistan" />
-                <GridFlow title="Minimum Salary" value="2000 Euro" />
+                <GridFlow title="Birth Date" value={moment(new Date(data?.dateOfBirth)).format('DD MMM YYYY')} />
+                <GridFlow title="Gender" value={data?.gender} />
+                <GridFlow title="Experience" value={`${data?.yearOfExperience} Years`} />
+                <GridFlow title="Country" value={capitalizeFirstLetter(data?.residenceCountry)} />
+                <GridFlow title="Minimum Salary" value={`${data?.minSalary} ${data?.currency}`} />
                 <GridFlow title="Profile Visibility" value="Registered only" />
-                <GridFlow title="City" value="Rawalpindi" />
-                <GridFlow title="Mobile" value="+923365121663" />
-                <GridFlow title="Email" value="shahbazrock0321@gmail.com" />
-                <GridFlow title="Address" value="Street#15b House#12 Bahar Colony near Lakhan Chowk Chakri Road" />
+                <GridFlow title="City" value={capitalizeFirstLetter(data?.city)} />
+                <GridFlow title="Mobile" value={data?.phone} />
+                <GridFlow title="Email" value={data?.email} />
+                <GridFlow title="Address" value={data?.address} />
             </div>
         </WithEditWrapper>
     )
