c930c00590b4d2dba28a3808228998b12dba91e7
shahbaz <shahbazrock0321@gmail.com>
2021-10-05 11:46:48 +0500

    Refetch data if RefetchApiContext change value

--- components/pages/profile/user/leftside/certificates/CertificatesWrapper.tsx
+++ components/pages/profile/user/leftside/certificates/CertificatesWrapper.tsx
@@ -1,26 +1,45 @@
 import { getUserCertificates } from '@api/Certificates'
+import { RefetchApiContext } from '@context/RefetchApiContext'
 import React, { useEffect, useState } from 'react'
 import SimpleWrapper from '../common/SimpleWrapper'
 import Certificates from './Certificates'
 
 const CertificatesWrapper = () => {
     const [data, setData] = useState([])
+    const [refetchApi, setRefetchApi] = useState(false)
+
+    const contextValue = {
+        setRefetch: () => {
+            setRefetchApi(true)
+        }
+    }
+
 
     useEffect(() => {
+        if (refetchApi) {
+            getUserCertificatesApi()
+        }
+    }, [refetchApi])
 
-        //allCareers()
-        (async () => {
-            const res = await getUserCertificates()
-            if (!res?.error) {
-                setData(res?.data)
-            }
-        })()
+    useEffect(() => {
+        getUserCertificatesApi()
     }, [])
 
+
+    const getUserCertificatesApi = async () => {
+        const res = await getUserCertificates()
+        if (!res?.error) {
+            setData(res?.data)
+            setRefetchApi(false)
+        }
+    }
+
     return (
-        <SimpleWrapper title="Certificates">
-            <Certificates data={data} />
-        </SimpleWrapper>
+        <RefetchApiContext.Provider value={contextValue}>
+            <SimpleWrapper title="Certificates">
+                <Certificates data={data} />
+            </SimpleWrapper>
+        </RefetchApiContext.Provider>
     )
 }
 
