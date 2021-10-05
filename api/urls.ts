0d2b5bd722d83ab606722e6f798df2b9c136e043
shahbaz <shahbazrock0321@gmail.com>
2021-10-05 11:52:51 +0500

    Add userSkills api url

--- api/urls.ts
+++ api/urls.ts
@@ -3,3 +3,4 @@ export const registerUserUrl = `${hostUrl}/register/candidate/`
 export const loginUserUrl = `${hostUrl}/login/candidate/`
 export const getUserUrl = `${hostUrl}/users/me/`
 export const userCertificates = `${hostUrl}/candidate/certificates/`
+export const userSkills = `${hostUrl}/candidate/skills/`
