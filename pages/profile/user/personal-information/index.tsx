41d5d6752a816f7127576e9e35cd9402a52fd99f
shahbaz <shahbazrock0321@gmail.com>
2021-10-05 15:07:09 +0500

    Import PersonalInformation component

--- pages/profile/user/personal-information/index.tsx
+++ pages/profile/user/personal-information/index.tsx
@@ -1,9 +1,10 @@
+import PersonalInformation from '@components/pages/profile/user/personal-information/PersonalInformation'
 import React from 'react'
 
 const Index = () => {
     return (
         <div>
-            personal
+            <PersonalInformation />
         </div>
     )
 }
