00391336a82ca5317ec002fb00c21abee33a6327
shahbaz <shahbazrock0321@gmail.com>
2021-10-05 17:07:35 +0500

    Use query data

--- components/pages/profile/user/profile/leftside/TitleInfo.tsx
+++ components/pages/profile/user/profile/leftside/TitleInfo.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import BoxWrapper from '@components/common/boxWrapper/BoxWrapper'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import capitalizeFirstLetter from '@components/functions/capitalizeFirstLetter';
 
 
-const TitleInfo = ({ user }: any) => {
+const TitleInfo = ({ data }: any) => {
     return (
 
         <BoxWrapper>
+            {console.log('data', data)}
             <div className="grid grid-flow-col">
                 <div className="col-span-10">
                     <div className="text-3xl font-bold">
-                        {user.name}
+                        {`${capitalizeFirstLetter(data?.user.firstName)} ${capitalizeFirstLetter(data?.user.lastName)}`}
                     </div>
                     <div className="text-base subtitle-clr">
-                        {user.subtitle}
+                        {capitalizeFirstLetter(data?.jobTitle.name)}
                     </div>
                     <div className="flex mt-4">
                         <LocationOnIcon sx={{ width: 15 }} />
-                        <div className="ml-1">Pakistan</div>
+                        <div className="ml-1">{capitalizeFirstLetter(data?.residenceCountry)}</div>
                         <div className="subtitle-clr ml-5 space-y-2">
                             <div className="flex items-center space-x-2">
                                 <div className="rounded-lg w-2 h-2 bg-[#3DD598]">
@@ -37,7 +39,6 @@ const TitleInfo = ({ user }: any) => {
                     </div>
                 </div>
                 <div className="col-span-2">
-                    profile
                 </div>
             </div>
 
