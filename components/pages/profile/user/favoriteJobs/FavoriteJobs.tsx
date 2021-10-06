c03602b8a5d2cb79e80faebf795c0accb3b692dc
shahbaz <shahbazrock0321@gmail.com>
2021-10-06 20:16:30 +0500

    Make FavoriteJobs component

---
+++ components/pages/profile/user/favoriteJobs/FavoriteJobs.tsx
@@ -0,0 +1,28 @@
+import Container from '@components/common/container/Container'
+import { url_userProfile } from '@components/functions/pageUrls'
+import router from 'next/router'
+import React from 'react'
+import JobCard from '../common/JobCard'
+import JobsHeader from '../common/JobsHeader'
+
+
+const FavoriteJobs = () => {
+    return (
+        <div className="gray-bg min-h-[100vh]">
+            <Container>
+                <div className="py-10">
+                    <div className="grid grid-cols-5">
+                        <div className="col-start-2 col-span-3">
+                            <JobsHeader title="Applied Jobs" onBack={() => router.push(url_userProfile)} />
+                            <div className="mt-7">
+                                <JobCard />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default FavoriteJobs
