db3ace48c15fe9fac4463f6bd171561958222eb5
shahbaz <shahbazrock0321@gmail.com>
2021-10-06 18:43:11 +0500

    Add JobCard component

--- components/pages/profile/user/appliedJobs/AppliedJobs.tsx
+++ components/pages/profile/user/appliedJobs/AppliedJobs.tsx
@@ -1,19 +1,25 @@
 import Container from '@components/common/container/Container'
 import React from 'react'
+import JobCard from '../common/JobCard'
 import JobsHeader from '../common/JobsHeader'
 
 
 const AppliedJobs = () => {
     return (
-        <Container>
-            <div className="py-10">
-                <div className="grid grid-cols-5">
-                    <div className="col-start-2 col-span-3">
-                        <JobsHeader title="Applied Jobs" onBack={() => console.log('back')} />
+        <div className="gray-bg min-h-[100vh]">
+            <Container>
+                <div className="py-10">
+                    <div className="grid grid-cols-5">
+                        <div className="col-start-2 col-span-3">
+                            <JobsHeader title="Applied Jobs" onBack={() => console.log('back')} />
+                            <div className="mt-7">
+                                <JobCard />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </div>
     )
 }
 
