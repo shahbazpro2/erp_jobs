1f1b0ba1ec55867933ba67ed150efd73a78aa597
shahbaz <shahbazrock0321@gmail.com>
2021-10-06 15:51:36 +0500

    Make applied-jobs page

---
+++ pages/profile/user/applied-jobs.tsx
@@ -0,0 +1,12 @@
+import AppliedJobs from '@components/pages/profile/user/appliedJobs/AppliedJobs'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            <AppliedJobs />
+        </div>
+    )
+}
+
+export default Index
