d641be9c0ac58ee1866f7a8855b6d5a1f8872fbb
shahbaz <shahbazrock0321@gmail.com>
2021-10-06 20:16:19 +0500

    Make favorite-jobs page

---
+++ pages/profile/user/favorite-jobs.tsx
@@ -0,0 +1,12 @@
+import FavoriteJobs from '@components/pages/profile/user/favoriteJobs/FavoriteJobs'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            <FavoriteJobs />
+        </div>
+    )
+}
+
+export default Index
