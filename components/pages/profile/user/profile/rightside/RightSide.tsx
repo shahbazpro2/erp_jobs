5d360c189ab3bba6da78010cfe1f4b61430b07dc
shahbaz <shahbazrock0321@gmail.com>
2021-10-06 14:12:03 +0500

    Import JobApplications and FavoriteJobs components=

--- components/pages/profile/user/profile/rightside/RightSide.tsx
+++ components/pages/profile/user/profile/rightside/RightSide.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Completion from './completion/Completion'
+import FavoriteJobs from './FavoriteJobs'
+import JobApplications from './JobApplications'
 
 const RightSide = () => {
     return (
-        <div>
+        <div className="space-y-4">
             <Completion />
+            <JobApplications />
+            <FavoriteJobs />
         </div>
     )
 }
