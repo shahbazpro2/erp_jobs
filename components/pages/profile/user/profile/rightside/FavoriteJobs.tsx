ef64050185239f49cce4d24efc4ce0ab673a0002
shahbaz <shahbazrock0321@gmail.com>
2021-10-06 20:16:03 +0500

    Make FavoriteJobs component

--- components/pages/profile/user/profile/rightside/FavoriteJobs.tsx
+++ components/pages/profile/user/profile/rightside/FavoriteJobs.tsx
@@ -1,9 +1,11 @@
+import { url_favoriteJobs } from '@components/functions/pageUrls'
+import router from 'next/router'
 import React from 'react'
 import WithBadgeWrapper from './WithBadgeWrapper'
 
 const FavoriteJobs = () => {
     return (
-        <WithBadgeWrapper content="My Favorite Jobs" badgeValue={2} onHandleClick={() => console.log('fav')} />
+        <WithBadgeWrapper content="My Favorite Jobs" badgeValue={2} onHandleClick={() => router.push(url_favoriteJobs)} />
     )
 }
 
