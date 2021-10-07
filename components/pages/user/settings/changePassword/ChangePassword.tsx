5b8a2848f5cf8d1bad5f175ecaacc1ac1c66351f
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 20:14:10 +0500

    Add onSubmit function and loading state

--- components/pages/user/settings/changePassword/ChangePassword.tsx
+++ components/pages/user/settings/changePassword/ChangePassword.tsx
@@ -3,18 +3,26 @@ import EmptyFieldCheck from '@components/functions/emptyFieldCheck'
 import FeedbackApi from '@components/common/feedback/FeedbackAPi'
 import ChangePasswordInputs from './ChangePasswordInputs'
 import validateEmail from '@components/functions/emailValidation'
+import { apiPasswordChange } from '@api/userSettings'
+import ObjectToArray from '@components/functions/ObjectToArray'
 
 
 export interface ChangePasswordProps {
-    oldPassword: string,
-    newPassword: string
+    old_password: string,
+    new_password1: string,
+    new_password2: string
+}
+
+const initialState = {
+    old_password: '', new_password1: '', new_password2: ''
 }
 
 const ChangePassword = () => {
-    const [state, setState] = useState<ChangePasswordProps>({ oldPassword: '', newPassword: '' })
+    const [state, setState] = useState<ChangePasswordProps>(initialState)
     const [inputError, setInputError] = useState(false)
     const [apiSuccess, setApiSuccess] = useState<string[]>([])
     const [apiError, setApiError] = useState<string[]>([])
+    const [loading, setLoading] = useState(false)
 
 
     const onSubmit = async (e: SyntheticEvent) => {
@@ -25,12 +33,30 @@ const ChangePassword = () => {
             setInputError(true)
             return
         }
+
+        if (state.old_password === state.new_password1) {
+            setApiError(['New password must be different with old passord'])
+            return
+        }
+        setLoading(true)
+
+        const res = await apiPasswordChange(state)
+        console.log('res', res?.data)
+        if (!res?.error) {
+            setApiSuccess(res?.data)
+            setLoading(false)
+            return
+        }
+
+        setApiError(res?.data)
+        setLoading(false)
+
     }
 
     return (
         <>
 
-            <ChangePasswordInputs state={state} setState={setState} inputError={inputError} onSubmit={onSubmit} />
+            <ChangePasswordInputs state={state} setState={setState} inputError={inputError} onSubmit={onSubmit} loading={loading} />
 
             <FeedbackApi setApiError={setApiError} setApiSuccess={setApiSuccess} apiError={apiError} apiSuccess={apiSuccess} />
         </>
