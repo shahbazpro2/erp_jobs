5bc8668058774e09f99c96381df57d860aa2f9bc
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 18:19:54 +0500

    Make Notification component

---
+++ components/pages/user/settings/notification/Notification.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Notification = () => {
+    return (
+        <div>
+            <div className="text-2xl">
+                Change Notification Settings
+            </div>
+            <div className="mt-3 subtitle-clr">
+                Do you want to receive emails to inform you of the latest suitable jobs ?
+            </div>
+            <div className="mt-5">
+
+            </div>
+        </div>
+    )
+}
+
+export default Notification
