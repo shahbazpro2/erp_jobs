2f39c67b20cef0715833594faa26e0e796d398d9
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 19:18:30 +0500

    Add checkbox

--- components/pages/user/settings/notification/Notification.tsx
+++ components/pages/user/settings/notification/Notification.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import { Checkbox, FormControlLabel } from '@mui/material'
+import React, { useState } from 'react'
 
 const Notification = () => {
+    const [check, setCheck] = useState(false)
     return (
-        <div>
+        <div className="font-semibold">
             <div className="text-2xl">
                 Change Notification Settings
             </div>
             <div className="mt-3 subtitle-clr">
                 Do you want to receive emails to inform you of the latest suitable jobs ?
             </div>
-            <div className="mt-5">
+            <div className="flex mt-5">
+                <FormControlLabel control={<Checkbox color="primary" checked={check} onChange={() => setCheck(true)} />} label="Yes" />
+                <FormControlLabel control={<Checkbox color="primary" checked={!check} onChange={() => setCheck(false)} />} label="No" />
 
             </div>
         </div>
