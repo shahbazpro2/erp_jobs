7ea1ba6ad90b700731f0b1431edeb689c9fd8201
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 15:27:04 +0500

    Add download and delete functions and show all cv

--- components/pages/user/profile/rightside/UploadCv.tsx
+++ components/pages/user/profile/rightside/UploadCv.tsx
@@ -4,22 +4,46 @@ import Image from 'next/image'
 import { Button, Input } from '@mui/material'
 import SimCardDownloadIcon from '@mui/icons-material/SimCardDownload';
 import ClearIcon from '@mui/icons-material/Clear';
-import { getCv, uploadCv } from '@api/CVUpload';
+import { deleteCv, getCv, uploadCv } from '@api/CVUpload';
 import FeedbackApi from '@components/common/feedback/FeedbackAPi';
 import LoadingButton from '@mui/lab/LoadingButton';
+import stringShort from '@components/functions/stringShort';
+import { saveAs } from 'file-saver';
+import DialogAlert from '@components/common/alerts/DialogAlert';
+
+interface CV {
+    id: number,
+    name: string,
+    file: string
+}
 
 const UploadCv = () => {
     const [loading, setLoading] = useState(false)
+    const [allCv, setAllCv] = useState<CV[]>([])
     const [apiSuccess, setApiSuccess] = useState<string[]>([])
     const [apiError, setApiError] = useState<string[]>([])
+    const [delId, setDelId] = useState<number>(-1)
 
 
     useEffect(() => {
         (async () => {
             const res = await getCv();
-            console.log('res', res)
+            let data: CV[] = []
+            if (!res?.error) {
+                res?.data?.map((cv: any) => {
+                    const name = cv.file?.split('/')
+                    if (name.length) {
+                        data.push({
+                            id: cv.id,
+                            name: name.pop(),
+                            file: cv.file
+                        })
+                    }
+                })
+            }
+            setAllCv(data)
         })()
-    }, [])
+    }, [apiSuccess])
 
     const onUploadCv = async (e: React.ChangeEvent<HTMLInputElement>) => {
         setLoading(true)
@@ -33,6 +57,7 @@ const UploadCv = () => {
 
         if (res?.error) {
             setApiError(res?.data)
+            setLoading(false)
             return
         }
         setLoading(false)
@@ -40,6 +65,17 @@ const UploadCv = () => {
 
     }
 
+    const onDelete = async () => {
+        const id = delId
+        setDelId(-1)
+        const res = await deleteCv(id)
+        if (!res?.error) {
+            setApiSuccess(['Cv deleted successfully'])
+            return
+        }
+
+        setApiError(res?.data)
+    }
 
 
     return (
@@ -51,17 +87,22 @@ const UploadCv = () => {
                         Upload CV Files
                     </div>
                 </div>
-                <div className="space-y-3">
-                    <div className="flex items-center justify-between">
-                        <div className="text-base subtitle-clr">
-                            Roxie Ward
+
+                <div className="space-y-4">
+                    {allCv.map((cv, index) => (
+                        <div key={index} className="flex items-center justify-between">
+                            <div className="text-base subtitle-clr">
+                                {stringShort(cv.name, 20)}
+                            </div>
+                            <div className="action subtitle-clr space-x-2">
+                                <SimCardDownloadIcon onClick={() => saveAs(cv.file, cv.name)} className="cursor-pointer success-clr-hover" sx={{ width: '18px' }} />
+                                <ClearIcon onClick={() => setDelId(cv.id)} className="cursor-pointer danger-clr-hover" />
+                            </div>
                         </div>
-                        <div className="action subtitle-clr space-x-2">
-                            <SimCardDownloadIcon className="cursor-pointer success-clr-hover" sx={{ width: '18px' }} />
-                            <ClearIcon className="cursor-pointer danger-clr-hover" />
-                        </div>
-                    </div>
+                    ))}
                 </div>
+
+
                 <div>
                     <label htmlFor="contained-button-file">
                         <Input onChange={onUploadCv} id="contained-button-file" sx={{ display: 'none' }} type="file" />
@@ -70,6 +111,7 @@ const UploadCv = () => {
                 </div>
 
             </BoxWrapper>
+            <DialogAlert open={delId < 0 ? false : true} title="Are to sure to delete cv?" handleClose={() => setDelId(-1)} handleAccept={onDelete} />
             <FeedbackApi setApiError={setApiError} setApiSuccess={setApiSuccess} apiError={apiError} apiSuccess={apiSuccess} />
         </div>
     )
