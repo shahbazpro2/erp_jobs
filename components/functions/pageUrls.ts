2217a50c6a427fcf73b863dca2c58e466a0bcf90
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 17:29:06 +0500

    Add url_userSettings page url

--- components/functions/pageUrls.ts
+++ components/functions/pageUrls.ts
@@ -2,3 +2,4 @@ export const url_userJobApplications = '/user/applied-jobs'
 export const url_userProfile = '/user/profile'
 export const url_userFavoriteJobs = '/user/favorite-jobs'
 export const url_userPersonalInformation = "/user/personal-information"
+export const url_userSettings = "/user/settings"
