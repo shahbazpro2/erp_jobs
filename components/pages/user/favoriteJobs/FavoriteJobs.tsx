0f0db5ab3330bba6e550c9be702f84467dc49797
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 13:09:15 +0500

    Replace grid with ShrinkContainer

--- components/pages/user/favoriteJobs/FavoriteJobs.tsx
+++ components/pages/user/favoriteJobs/FavoriteJobs.tsx
@@ -1,4 +1,5 @@
 import Container from '@components/common/container/Container'
+import ShrinkContainer from '@components/common/container/ShrinkContainer'
 import { url_userProfile } from '@components/functions/pageUrls'
 import router from 'next/router'
 import React from 'react'
@@ -9,18 +10,12 @@ import JobsHeader from '../common/JobsHeader'
 const FavoriteJobs = () => {
     return (
         <div className="gray-bg min-h-[100vh]">
-            <Container>
-                <div className="py-10">
-                    <div className="grid grid-cols-5">
-                        <div className="col-start-2 col-span-3">
-                            <JobsHeader title="Saved Jobs" onBack={() => router.push(url_userProfile)} />
-                            <div className="mt-7">
-                                <JobCard saved={true} />
-                            </div>
-                        </div>
-                    </div>
+            <ShrinkContainer>
+                <JobsHeader title="Saved Jobs" onBack={() => router.push(url_userProfile)} />
+                <div className="mt-7">
+                    <JobCard saved={true} />
                 </div>
-            </Container>
+            </ShrinkContainer>
         </div>
     )
 }
