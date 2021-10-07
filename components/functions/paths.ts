1a9110970bc8c93342316d61cf6b2e96962b371d
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 19:39:05 +0500

    Add url_userSettings path

--- components/functions/paths.ts
+++ components/functions/paths.ts
@@ -1,11 +1,11 @@
-import { url_userPersonalInformation, url_userProfile } from "./pageUrls";
+import { url_userPersonalInformation, url_userProfile, url_userSettings } from "./pageUrls";
 
 interface Props {
     pathname: string
 }
 
 export const isProtectedRoute = ({ pathname }: Props) => {
-    return ["/test", url_userProfile, url_userPersonalInformation].includes(pathname)
+    return ["/test", url_userProfile, url_userPersonalInformation, url_userSettings].includes(pathname)
 }
 
 // Determines whether the route is a login route
