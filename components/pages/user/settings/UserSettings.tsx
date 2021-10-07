067c36f8477d0ce1e1f69b62005a6b9c22d992fd
shahbaz <shahbazrock0321@gmail.com>
2021-10-07 17:29:57 +0500

    Refactor right side items and add active class color

--- components/pages/user/settings/UserSettings.tsx
+++ components/pages/user/settings/UserSettings.tsx
@@ -1,28 +1,35 @@
 import BoxWrapper from '@components/common/boxWrapper/BoxWrapper'
 import ShrinkContainer from '@components/common/container/ShrinkContainer'
-import React from 'react'
+import React, { useState } from 'react'
 import ChangeEmail from './changeEmail/ChangeEmail'
+import classNames from 'classnames'
+
+
+const menuItems = [['Change Email', 'changeEmail'], ['Change Password', 'changePassword'], ['Notification', 'notification'], ['Other Setting', 'otherSetting']]
 
 const UserSettings = () => {
+    const [currentSetting, setCurrentSetting] = useState('changeEmail')
+
+
+
     return (
         <div className="gray-bg min-h-[100vh]">
             <ShrinkContainer>
                 <div className="text-3xl font-bold">
                     Setting
                 </div>
-                <div className="grid grid-cols-6 gap-3 py-7">
-                    <div className="col-span-4">
+                <div className="grid grid-cols-12 gap-3 py-7">
+                    <div className="col-span-9">
                         <BoxWrapper>
                             <ChangeEmail />
                         </BoxWrapper>
                     </div>
-                    <div className="col-span-2">
+                    <div className="col-span-3">
                         <BoxWrapper>
                             <div className="space-y-3 font-bold">
-                                <div className="cursor-pointer primary-clr-hover">Change Email</div>
-                                <div className="cursor-pointer primary-clr-hover">Change Password</div>
-                                <div className="cursor-pointer primary-clr-hover">Notification Setting</div>
-                                <div className="cursor-pointer primary-clr-hover">Other Setting</div>
+                                {menuItems.map((item, index) => (
+                                    <div key={index} className={classNames("cursor-pointer primary-clr-hover", { 'primary-clr': currentSetting === item[1] })} onClick={() => setCurrentSetting(item[1])}>{item[0]}</div>
+                                ))}
                             </div>
                         </BoxWrapper>
                     </div>
