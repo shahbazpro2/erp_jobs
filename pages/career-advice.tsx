1bf2b0fdda50f40810bbf6241516ebd87f2c1385
shahbaz <shahbazrock0321@gmail.com>
2021-10-08 20:06:12 +0500

    Add title

--- pages/career-advice.tsx
+++ pages/career-advice.tsx
@@ -1,9 +1,13 @@
 import CareerAdvice from '@components/pages/careerAdvice/CareerAdvice'
 import React from 'react'
+import Head from 'next/head'
 
 const Index = () => {
     return (
         <div>
+            <Head>
+                <title>Career Advice</title>
+            </Head>
             <CareerAdvice />
         </div>
     )
