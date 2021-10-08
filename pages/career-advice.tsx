add2449aa9c091d6e8e2e208575fb459c46c8dd2
shahbaz <shahbazrock0321@gmail.com>
2021-10-08 20:05:22 +0500

    Make career-advice page

---
+++ pages/career-advice.tsx
@@ -0,0 +1,12 @@
+import CareerAdvice from '@components/pages/careerAdvice/CareerAdvice'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            <CareerAdvice />
+        </div>
+    )
+}
+
+export default Index
