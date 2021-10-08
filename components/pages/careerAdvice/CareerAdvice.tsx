c0759e94fcc2789b7b309c3dc0105d5002ca134e
shahbaz <shahbazrock0321@gmail.com>
2021-10-08 20:05:10 +0500

    Make CareerAdvice component

---
+++ components/pages/careerAdvice/CareerAdvice.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+
+const CareerAdvice = () => {
+    return (
+        <div>
+            <div className="grid grid-cols-8">
+                <div className="col-start-3 col-span-4">
+                    <div className="py-20 text-center">
+                        <div className="text-5xl font-bold">
+                            Career Advice
+                        </div>
+                        <div className="text-xl mt-7">
+                            Whether you're looking to change careers or simply want to know what interview questions to prepare for, this is the place for career advice and tips.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default CareerAdvice
