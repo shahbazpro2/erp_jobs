cc47474ea638b17896b92bfe90adad48b234af32
shahbaz <shahbazrock0321@gmail.com>
2021-10-08 20:08:25 +0500

    Add url_careerAdvice

--- components/functions/pageUrls.ts
+++ components/functions/pageUrls.ts
@@ -3,3 +3,4 @@ export const url_userProfile = '/user/profile'
 export const url_userFavoriteJobs = '/user/favorite-jobs'
 export const url_userPersonalInformation = "/user/personal-information"
 export const url_userSettings = "/user/settings"
+export const url_careerAdvice = "/career-advice"
