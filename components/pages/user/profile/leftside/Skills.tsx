d9b37895a64dcddfbf5e15c05da2fc22a9c8b18d
shahbaz <shahbazrock0321@gmail.com>
2021-10-08 15:40:53 +0500

    Get Skills and update Autocomplete

--- components/pages/user/profile/leftside/Skills.tsx
+++ components/pages/user/profile/leftside/Skills.tsx
@@ -1,34 +1,52 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { Dispatch, Fragment, SetStateAction, useEffect, useState } from 'react'
-import { Autocomplete, Button, Chip, Divider, TextField } from '@mui/material';
-import AddItemsWrapper from '@components/common/addItemsWrapper/AddItemsWrapper';
+import React, { Fragment, useEffect, useState } from 'react'
+import { Autocomplete, Chip, TextField } from '@mui/material';
 import { useLazyQuery, useMutation } from '@apollo/client';
 import FeedbackApi from '@components/common/feedback/FeedbackAPi';
 import { AllSkills } from '@graphql/queries/common/AllSkills';
 import { CreateSkills } from '@graphql/mutations/user/skills/CreateSkills';
 import { CandidateSkills } from '@graphql/queries/user/skills/CandidateSkills';
-import graphqlRes from '@components/functions/graphqlRes';
 import SimpleWrapper from './common/SimpleWrapper';
-
+import { addUserSkills, getUserSkills } from '@api/skills';
+import { useAppSelector } from '@redux/Store';
+import { LoadingButton } from '@mui/lab'
 
 
 type AllSkill = {
     id: string,
     name: string
 }
-
+type PostSkills = {
+    user: string,
+    skills: number[]
+}
 const Skills = () => {
+    const user: any = useAppSelector(state => state.authReducer)
     const [allSkillsFetch, { data }] = useLazyQuery(AllSkills)
-    const [createSkills] = useMutation(CreateSkills, { refetchQueries: [CandidateSkills], onError: () => null })
     const [allSkills, setAllSkills] = useState<AllSkill[]>([])
     const [value, setValue] = React.useState<any>([]);
     const [apiError, setApiError] = useState<string[]>([])
     const [apiSuccess, setApiSuccess] = useState<string[]>([])
+    const [loading, setLoading] = useState(false)
 
 
     useEffect(() => {
-        if (data?.allSkills)
-            setAllSkills(data?.allSkills)
+        if (data?.allSkills) {
+            setAllSkills(data?.allSkills);
+
+            (async () => {
+                const res = await getUserSkills()
+
+                const filterSkills = []
+                for (const all of data?.allSkills) {
+                    for (const resSkill of res?.data[0]?.skills) {
+                        if (Number(all.id) === Number(resSkill))
+                            filterSkills.push(all)
+                    }
+                }
+                setValue(filterSkills)
+            })()
+        }
     }, [data])
 
 
@@ -37,8 +55,28 @@ const Skills = () => {
     }, [])
 
 
-    const addNewSkills = () => {
 
+    const addNewSkills = async () => {
+
+        const skillsId = value.map((f: AllSkill) => {
+            return Number(f.id)
+        })
+
+        const skillsData: PostSkills = {
+            user: user?.user?.id,
+            skills: skillsId
+        }
+        setLoading(true)
+        const res = await addUserSkills(skillsData)
+
+        if (!res?.error) {
+            setApiSuccess(['Skills updated successfully'])
+            setLoading(false)
+        }
+        setLoading(false)
+        setApiError(res?.data)
+
+        console.log('res', res)
     }
 
 
@@ -49,8 +87,11 @@ const Skills = () => {
                     <Autocomplete
                         disableClearable
                         multiple
+                        value={value}
                         id="multiple-limit-tags"
-                        onChange={(event, newValue) => setValue(newValue)}
+                        onChange={(event, newValue) => {
+                            setValue(newValue)
+                        }}
                         options={allSkills}
                         getOptionLabel={(option) => option.name}
                         renderInput={(params) => (
@@ -62,7 +103,7 @@ const Skills = () => {
                     />
                 </div>
                 <div className="mt-7">
-                    <Button variant="outlined" fullWidth>Update Skills</Button>
+                    <LoadingButton loading={loading} variant="outlined" fullWidth onClick={addNewSkills}>Update Skills</LoadingButton>
                 </div>
             </SimpleWrapper>
             <FeedbackApi apiError={apiError} apiSuccess={apiSuccess} setApiError={setApiError} setApiSuccess={setApiSuccess} />
