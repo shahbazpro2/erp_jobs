6519c2780bac50df941a34e3abb1fe3cff22782b
shahbaz <shahbazrock0321@gmail.com>
2021-10-11 19:20:08 +0500

    Add login and register user path

--- components/functions/pageUrls.ts
+++ components/functions/pageUrls.ts
@@ -1,3 +1,5 @@
+export const url_registerUser = '/register/user'
+export const url_loginUser = '/login/user'
 export const url_userJobApplications = '/user/applied-jobs'
 export const url_userProfile = '/user/profile'
 export const url_userFavoriteJobs = '/user/favorite-jobs'
