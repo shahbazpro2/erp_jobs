85521cfee09b55f0ff43931f84976c7862fed09a
shahbaz <shahbazrock0321@gmail.com>
2021-10-12 18:38:04 +0500

    Padding change when screen small

--- components/common/boxWrapper/BoxWrapper.tsx
+++ components/common/boxWrapper/BoxWrapper.tsx
@@ -4,7 +4,7 @@ interface Props {
     children: React.ReactNode,
     className?: string,
 }
-const BoxWrapper = ({ children, className = "px-7 py-8" }: Props) => {
+const BoxWrapper = ({ children, className = "px-5  md:px-7 py-8" }: Props) => {
     return (
         <div className={"bg-white border-[1px] border-[#D9D9D9]  rounded-[7px] " + className}>
             {children}
