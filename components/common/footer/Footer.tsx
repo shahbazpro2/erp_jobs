4a3d85ae3e099c01e866c652c9bc3d119358e3b0
shahbaz <shahbazrock0321@gmail.com>
2021-10-12 13:32:00 +0500

    Make Footer component responsive

--- components/common/footer/Footer.tsx
+++ components/common/footer/Footer.tsx
@@ -1,54 +1,55 @@
 import React from 'react'
 import Image from 'next/image'
+import Container from '@components/common/container/Container'
 
 const Footer = () => {
     return (
-        <div className="bg-[#161C2D] py-32 text-white">
-            <div className="grid grid-cols-12">
-                <div className="col-start-3 col-span-8">
-                    <div className="grid grid-cols-6 gap-4">
-                        <div className="col-span-2">
-                            <div className="text-3xl font-bold logo">
-                                ERP Jobs
+        <div className="bg-[#161C2D] md:py-32 py-20 text-white">
+            <Container>
+                <div className="grid grid-cols-8 md:grid-cols-6 md:grid-rows-1 grid-rows-2 gap-4">
+                    <div className="md:col-span-2 col-span-8">
+                        <div className="text-3xl font-bold logo">
+                            ERP Jobs
+                        </div>
+                        <div className="mt-5 text-base opacity-[0.6]">
+                            Millions of people are searching for jobs, salary information, company reviews, and interview questions on this website.
+                        </div>
+                        <div className="flex md:mt-10 sm:mt-5  space-x-5">
+                            <div className="cursor-pointer">
+                                <Image src="/assets/images/twitter.svg" width="18" height="18" alt="facebook" />
                             </div>
-                            <div className="mt-5 text-base opacity-[0.6]">
-                                Millions of people are searching for jobs, salary information, company reviews, and interview questions on this website.
-                            </div>
-                            <div className="flex mt-16 space-x-5">
-                                <div className="cursor-pointer">
-                                    <Image src="/assets/images/twitter.svg" width="18" height="18" alt="facebook" />
-                                </div>
-                                <div className="cursor-pointer">
+                            <div className="cursor-pointer">
 
-                                    <Image src="/assets/images/facebook.svg" width="18" height="18" alt="facebook" />
-                                </div>
-                                <div className="cursor-pointer">
-                                    <Image src="/assets/images/instagram.svg" width="18" height="18" alt="facebook" />
-                                </div>
+                                <Image src="/assets/images/facebook.svg" width="18" height="18" alt="facebook" />
+                            </div>
+                            <div className="cursor-pointer">
+                                <Image src="/assets/images/instagram.svg" width="18" height="18" alt="facebook" />
                             </div>
                         </div>
-                        <div >
-                            <div className="text-base opacity-[0.6]">
-                                Company
-                            </div>
-                            <div className="mt-5 space-y-3">
-                                <div>About us</div>
-                                <div>Contact us</div>
-                                <div>Careers us</div>
-                            </div>
+                    </div>
+                    <div className="col-span-2 md:col-auto">
+                        <div className="text-base opacity-[0.6]">
+                            Company
                         </div>
-                        <div >
-                            <div className="text-base opacity-[0.6]">
-                                Product
-                            </div>
-                            <div className="mt-5 space-y-3">
-                                <div>Features</div>
-                                <div>Pricing</div>
-                                <div>News</div>
-                                <div>Support</div>
-                            </div>
+                        <div className="mt-5 space-y-3">
+                            <div>About us</div>
+                            <div>Contact us</div>
+                            <div>Careers us</div>
                         </div>
-                        <div >
+                    </div>
+                    <div className="col-span-1 md:col-auto">
+                        <div className="text-base opacity-[0.6]">
+                            Product
+                        </div>
+                        <div className="mt-5 space-y-3">
+                            <div>Features</div>
+                            <div>Pricing</div>
+                            <div>News</div>
+                            <div>Support</div>
+                        </div>
+                    </div>
+                    <div className="col-span-3 md:col-auto md:block justify-center flex">
+                        <div>
                             <div className="text-base opacity-[0.6]">
                                 Services
                             </div>
@@ -58,20 +59,20 @@ const Footer = () => {
                                 <div>Employers</div>
                             </div>
                         </div>
-                        <div >
-                            <div className="text-base opacity-[0.6]">
-                                Company
-                            </div>
-                            <div className="mt-5 space-y-3">
-                                <div>Privacy Policy</div>
-                                <div>Terms & Conditions</div>
-                                <div>Return Policy</div>
-                            </div>
+                    </div>
+                    <div className="col-span-2 md:col-auto">
+                        <div className="text-base opacity-[0.6]">
+                            Company
+                        </div>
+                        <div className="mt-5 space-y-3">
+                            <div>Privacy Policy</div>
+                            <div>Terms & Conditions</div>
+                            <div>Return Policy</div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </Container >
+        </div >
     )
 }
 
