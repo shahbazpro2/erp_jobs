bb248883ff4232f11e2b401e8ab25b4cf2794b54
shahbaz <shahbazrock0321@gmail.com>
2021-10-13 18:30:20 +0500

    Remove api calls

--- components/pages/user/profile/leftside/LeftSide.tsx
+++ components/pages/user/profile/leftside/LeftSide.tsx
@@ -1,27 +1,21 @@
 import CareerWrapper from './career/CareerWrapper'
-import React, { useEffect } from 'react'
+import React from 'react'
 import PersonalInfo from './personalInfo/PersonalInfo'
 import Summary from './summary/SummaryWrapper'
 import TitleInfo from './TitleInfo'
 import EducationWrapper from './education/EducationWrapper'
 import CertificatesWrapper from './certificates/CertificatesWrapper'
 import Skills from './Skills'
-import { useLazyQuery } from '@apollo/client'
-import { LoginCandidate } from '@graphql/queries/LoginCandidate'
-import { useAppSelector } from '@redux/Store'
 
 
-const LeftSide = () => {
-    const user: any = useAppSelector(state => state.authReducer)
-    const [loginCandidate, { data }] = useLazyQuery(LoginCandidate)
 
-    useEffect(() => {
-        loginCandidate()
-    }, [])
+const LeftSide = ({ data, user }: any) => {
 
     return (
         <div className="space-y-5">
-            <TitleInfo data={data?.loginCandidate} user={user?.user} />
+            <div className="hidden lg:block">
+                <TitleInfo data={data?.loginCandidate} user={user?.user} />
+            </div>
             <Summary />
             <PersonalInfo data={data?.loginCandidate} user={user?.user} />
             <CareerWrapper />
