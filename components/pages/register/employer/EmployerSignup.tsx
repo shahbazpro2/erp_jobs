9e9d993c2dd7affee5c79ed50b243a5094b938b0
shahbaz <shahbazrock0321@gmail.com>
2021-10-13 17:42:36 +0500

    Increase height and pass bg-transparent to handleHeader context

--- components/pages/register/employer/EmployerSignup.tsx
+++ components/pages/register/employer/EmployerSignup.tsx
@@ -1,23 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react'
 import { HeaderContext } from '@context/HeaderContext'
-import { Paper } from '@mui/material'
 
 const EmployerSignup = () => {
     const context = useContext(HeaderContext)
 
     useEffect(() => {
-        context.handleHeader('bg-none', 'none', true)
+        context.handleHeader('bg-transparent', 'none', true)
         return (() => {
             context.handleHeader('bg-white', 'boxShadow', false)
         })
     }, [])
 
     return (
-        <Paper className="bg-mario w-full h-[700px]">
-            <div>
-                employer
-            </div>
-        </Paper>
+        <div className="bg-mario w-full h-[1000px]">
+        </div>
     )
 }
 
