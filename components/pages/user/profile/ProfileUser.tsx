5f80c15ac588675cabbdfc8c8aaa1d2c60ae5a97
shahbaz <shahbazrock0321@gmail.com>
2021-10-13 18:30:08 +0500

    Call LoginCandidate query and get user from redux

--- components/pages/user/profile/ProfileUser.tsx
+++ components/pages/user/profile/ProfileUser.tsx
@@ -1,18 +1,28 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useLazyQuery } from '@apollo/client'
 import Container from '@components/common/container/Container'
-import React from 'react'
+import { LoginCandidate } from '@graphql/queries/LoginCandidate'
+import { useAppSelector } from '@redux/Store'
+import React, { useEffect } from 'react'
 import LeftSide from './leftside/LeftSide'
 import RightSide from './rightside/RightSide'
 
 const ProfileUser = () => {
+    const user: any = useAppSelector(state => state.authReducer)
+    const [loginCandidate, { data }] = useLazyQuery(LoginCandidate)
+
+    useEffect(() => {
+        loginCandidate()
+    }, [])
     return (
         <div className="bg-[#F2F2F2]">
             <Container>
-                <div className="grid lg:grid-cols-12 grid-cols-1 gap-6 py-16">
+                <div className="grid lg:grid-cols-12 grid-cols-1 grid-flow-row gap-6 py-16">
                     <div className="lg:col-span-8 xl:col-span-9">
-                        <LeftSide />
+                        <LeftSide data={data} user={user} />
                     </div>
-                    <div className="lg:col-span-4 xl:col-span-3">
-                        <RightSide />
+                    <div className="lg:col-span-4 xl:col-span-3 order-first lg:order-last">
+                        <RightSide data={data} user={user} />
                     </div>
                 </div>
             </Container>
