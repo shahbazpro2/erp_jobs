9537abc880732629cdbc4a345317a4167b062ee1
shahbaz <shahbazrock0321@gmail.com>
2021-10-13 18:31:18 +0500

    Add TitleInfo component for small screen

--- components/pages/user/profile/rightside/RightSide.tsx
+++ components/pages/user/profile/rightside/RightSide.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import TitleInfo from '../leftside/TitleInfo'
 import Completion from './completion/Completion'
 import FavoriteJobs from './FavoriteJobs'
 import JobApplications from './JobApplications'
 import UploadCv from './UploadCv'
 
-const RightSide = () => {
+const RightSide = ({ data, user }: any) => {
     return (
         <div className="space-y-4">
             <Completion />
+            <div className="block lg:hidden">
+                <TitleInfo data={data?.loginCandidate} user={user?.user} />
+            </div>
             <JobApplications />
             <FavoriteJobs />
             <UploadCv />
