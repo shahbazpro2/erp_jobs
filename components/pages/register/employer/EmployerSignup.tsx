cf4b54d17d2a626d08fc10ab305ec553a37e02dd
shahbaz <shahbazrock0321@gmail.com>
2021-10-18 13:15:36 +0500

    Add title and description

--- components/pages/register/employer/EmployerSignup.tsx
+++ components/pages/register/employer/EmployerSignup.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react'
 import { HeaderContext } from '@context/HeaderContext'
+import Container from '@components/common/container/Container'
 
 const EmployerSignup = () => {
     const context = useContext(HeaderContext)
@@ -13,7 +14,25 @@ const EmployerSignup = () => {
     }, [])
 
     return (
-        <div className="bg-mario w-full h-[1000px]">
+        <div className="bg-mario w-full h-[1000px] text-white">
+            <Container>
+                <div className="grid grid-cols-2">
+                    <div className="flex items-center h-screen">
+                        <div>
+                            <div className="text-3xl font-bold">
+                                Employer Account
+                            </div>
+                            <div className="text-2xl mt-7">
+                                ERP Jobs provides you with an advanced set of special candidate management tools where you can view and filter employees into different categories and folders, rank according to their level, and add any comments on each candidate so that the recruitment process is more organized and easy.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+
+                    </div>
+                </div>
+
+            </Container>
         </div>
     )
 }
