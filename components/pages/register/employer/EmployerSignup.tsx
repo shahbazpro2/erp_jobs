3aed09b2a2b7b9a800daeeff9ddc60c58381ce32
shahbaz <shahbazrock0321@gmail.com>
2021-10-18 17:31:58 +0500

    Add onSubmit and feedback

--- components/pages/register/employer/EmployerSignup.tsx
+++ components/pages/register/employer/EmployerSignup.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect, useState } from 'react'
+import React, { SyntheticEvent, useContext, useEffect, useState } from 'react'
 import { HeaderContext } from '@context/HeaderContext'
 import Container from '@components/common/container/Container'
 import BoxWrapper from '@components/common/boxWrapper/BoxWrapper'
@@ -10,19 +10,30 @@ import SocialsLogin from '@components/common/socialsLogin/SocialsLogin'
 import { Button } from '@mui/material'
 import EmployerAuthWrapper from '@components/pages/auth/EmployerAuthWrapper'
 import RegisterForm from '@components/common/forms/RegisterForm'
+import FeedbackApi from '@components/common/feedback/FeedbackAPi'
+import EmptyFieldCheck from '@components/functions/emptyFieldCheck'
+import validateEmail from '@components/functions/emailValidation'
+import { registerEmployer } from '@api/employerAuth'
+import { url_loginEmp } from '@components/functions/pageUrls'
+import router from 'next/router'
+
+
+const initialState = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    gender: '',
+    password1: '',
+    password2: ''
+}
 
 const EmployerSignup = () => {
     const context = useContext(HeaderContext)
-    const [state, setState] = useState({
-        firstname: '',
-        lastname: '',
-        email: '',
-        gender: '',
-        password1: '',
-        password2: ''
-    })
+    const [state, setState] = useState(initialState)
     const [loading, setLoading] = useState(false)
     const [inputError, setInputError] = useState(false)
+    const [apiError, setApiError] = useState<any[]>([])
+    const [apiSuccess, setApiSuccess] = useState<any[]>([])
 
     useEffect(() => {
         context.handleHeader('bg-transparent', 'none', true)
@@ -31,9 +42,38 @@ const EmployerSignup = () => {
         })
     }, [])
 
-    const onSubmit = async () => {
-        console.log('submit')
+    const onSubmit = async (e: SyntheticEvent) => {
+        e.preventDefault()
+        setInputError(false)
+        const { firstname, lastname, email, password1, password2, gender } = state
+
+        if (EmptyFieldCheck({ firstname, lastname, email, password1, password2, gender }) || !validateEmail(email) || password1 !== password2 || password1.length < 8) {
+            setInputError(true)
+            return
+        }
+        setLoading(true)
+        const data = {
+            first_name: firstname, last_name: lastname, email, password: password1, password2, gender
+        }
+
+        const res = await registerEmployer(data)
+        if (res?.error) {
+            setApiError(res.data)
+            setLoading(false)
+        } else {
+            console.log(res?.data)
+            localStorage.setItem('token', res?.data.token)
+            setTimeout(() => {
+                //router.push(url_userProfile)
+            }, 1000);
+            setState(initialState)
+            setApiSuccess(['User registered successfully'])
+            setLoading(false)
+
+        }
+
     }
+
 
     return (
         <>
@@ -58,11 +98,11 @@ const EmployerSignup = () => {
                     <SocialsLogin />
                 </BoxWrapper>
                 <div className="mt-7 text-white text-white-hover">
-                    <Button variant="outlined" color="inherit" fullWidth>
+                    <Button onClick={() => router.push(url_loginEmp)} variant="outlined" color="inherit" fullWidth>
                         <span className="text-base md:text-xl">Already have an account ? Sign In</span></Button>
                 </div>
             </EmployerAuthWrapper>
-
+            <FeedbackApi apiSuccess={apiSuccess} apiError={apiError} setApiError={() => setApiError([])} setApiSuccess={() => setApiSuccess([])} />
         </>
 
 
