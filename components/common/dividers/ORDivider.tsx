1d6250084ae0621cfd0b26209dde4eea079f0717
shahbaz <shahbazrock0321@gmail.com>
2021-10-18 13:13:07 +0500

    Make ORDIvider component

---
+++ components/common/dividers/ORDivider.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ORDivider = () => {
+    return (
+        <div className="my-6">
+            <div className="divider">
+                <span>or</span>
+            </div>
+        </div>
+    )
+}
+
+export default ORDivider
