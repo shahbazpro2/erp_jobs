769274c26dbeae519967a14813c8512084bd26e6
shahbaz <shahbazrock0321@gmail.com>
2021-10-20 17:49:24 +0500

    Fix undefined data passing to props

--- components/pages/user/profile/leftside/jobDetails/JobDetails.tsx
+++ components/pages/user/profile/leftside/jobDetails/JobDetails.tsx
@@ -16,8 +16,7 @@ const JobDetails = () => {
             const res = await getUserJobDetails();
             if (!res?.error) {
                 if (res?.data.length) {
-                    const { industry, desire_job_title } = res?.data[0]
-                    setData({ ...res?.data[0], industry: industry.name, desire_job_title: desire_job_title.name })
+                    setData({ ...res?.data[0] })
 
                 } else {
                     setData({ ...data, availability_date: '' })
@@ -30,8 +29,8 @@ const JobDetails = () => {
         <WithEditWrapper title="Job Details" onEditHandle={() => router.push(url_jobsDetails)}>
             <div className="grid grid-cols-2 gap-4">
                 <GridFlow title="Job Types" value={data?.job_type?.toString()} />
-                <GridFlow title="Industries" value={data?.industry} />
-                <GridFlow title="Desired Job" value={data?.desire_job_title} />
+                <GridFlow title="Industries" value={data?.industry?.name} />
+                <GridFlow title="Desired Job" value={data?.desire_job_title?.name} />
                 <GridFlow title="Date Available" value={data?.availability_date} />
                 <GridFlow title="Salary" value={`${data?.min_salary}-${data?.max_salary}`} />
             </div>
