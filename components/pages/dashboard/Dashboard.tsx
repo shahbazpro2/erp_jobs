37c24af81fbbd94ddf05da7e07bf3578b6b4c0b2
shahbaz <shahbazrock0321@gmail.com>
2021-10-21 17:52:31 +0500

    Make layout and import DetailCard compnent

--- components/pages/dashboard/Dashboard.tsx
+++ components/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,25 @@
+import Container from '@components/common/container/Container'
 import React from 'react'
+import DetailCard from './DetailCard'
+import PlanCard from './PlanCard'
 
 const Dashboard = () => {
     return (
-        <div>
-            dashboard
+        <div className="gray-bg">
+            <Container>
+                <div className="grid grid-cols-2">
+                    <div className="py-14">
+                        <div className="grid grid-cols-4 gap-5">
+
+                            <DetailCard title="Total Job Ads" count="09" />
+                            <DetailCard title="Active Ads" count="04" />
+                            <DetailCard title="Inactive Ads" count="05" />
+                            <PlanCard plan="Silver Package" cv="55" days="25" />
+
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </div>
     )
 }
