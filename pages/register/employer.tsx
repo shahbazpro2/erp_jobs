e1adad3b82609883cae86d135a1d9d09b53a1e5e
shahbaz <shahbazrock0321@gmail.com>
2021-10-21 15:43:51 +0500

    Check if url plan exist or not

--- pages/register/employer.tsx
+++ pages/register/employer.tsx
@@ -1,14 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { NextPage } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from 'next/head'
 import EmployerSignup from "@components/pages/register/employer/EmployerSignup";
+import { useRouter } from "next/router";
+import Spinner from "@components/common/spinner/Spinner";
+import { url_pricing } from "@components/functions/pageUrls";
 
 const Employer: NextPage = () => {
+    const [loading, setLoading] = useState(true)
+    const router = useRouter()
+
+    useEffect(() => {
+        !router.query.plan ? router.push(url_pricing) : setLoading(false)
+    }, [])
+
     return (<>
         <Head>
             <title>Register</title>
         </Head>
-        <EmployerSignup />
+        {loading ? <Spinner /> :
+            <EmployerSignup />}
     </>)
 };
 
