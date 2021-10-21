eead9d356e4175336044ccf33f2fca2ea08eec28
shahbaz <shahbazrock0321@gmail.com>
2021-10-21 15:51:12 +0500

    Make responsive

--- components/pages/pricing/Pricing.tsx
+++ components/pages/pricing/Pricing.tsx
@@ -1,4 +1,4 @@
-import ContainerPx from '@components/common/container/ContainerPx'
+import Container from '@components/common/container/Container'
 import React from 'react'
 import EnterpriseCard from './EnterpriseCard'
 import PricingCard from './PricingCard'
@@ -6,22 +6,22 @@ import PricingCard from './PricingCard'
 const Pricing = () => {
     return (
         <div className="silver-bg pt-24 pb-40">
-            <ContainerPx>
-                <div className="text-center">
+            <Container>
+                <div className="text-center xl:px-40">
                     <div className="text-5xl font-bold">
                         Pricing & Plans
                     </div>
-                    <div className="text-lg mt-7 w-[600px] m-auto opacity-70">
+                    <div className="text-lg mt-7 md:w-[600px] m-auto opacity-70">
                         All our plans include all erp-job features from A-Z. Our pricing is easy and only depends on the amount of unlocks.
                     </div>
-                    <div className="grid grid-cols-3 mt-24 gap-10">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-24 gap-10">
                         <PricingCard list={['100£ Per Job', '50 CV Downloads', '30 Days Validation Period', 'Premium Support']} plan="silver" price="100" btnColor="secondary" />
                         <PricingCard list={['130£ Per Job', '80 CV Downloads', '30 Days Validation Period', 'Premium Support']} plan="golden" price="130" btnColor="error" />
                         <EnterpriseCard list={['130£ Per Job', '80 CV Downloads', '30 Days Validation Period', 'Premium Support']} />
                     </div>
                 </div>
 
-            </ContainerPx>
+            </Container>
         </div>
     )
 }
