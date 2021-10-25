70c0b175d9ee8202e31caecdedd1087a6ec61fae
shahbaz <shahbazrock0321@gmail.com>
2021-10-25 16:39:49 +0500

    Make companyInformation api page

---
+++ api/employer/companyInformation.ts
@@ -0,0 +1,10 @@
+import { empCompanyInformation } from "@api/urls"
+import responseApi from "../responseApi"
+
+
+export const addCompanyInformation = async (data: FormData) => {
+    return responseApi(empCompanyInformation, 'post', data)
+}
+
+
+
