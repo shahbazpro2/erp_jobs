3b8b5f45117d1bb648cc1a3b5c8b3a1c55073594
shahbaz <shahbazrock0321@gmail.com>
2021-10-25 14:52:36 +0500

    Make types

---
+++ components/pages/dashboard/editCompanyDetails/types.ts
@@ -0,0 +1,13 @@
+
+export interface EditCompanyDetailsProps {
+    name: string,
+    type: string,
+    size: string,
+    industry: string,
+    country: string,
+    state: string,
+    address: string,
+    about: string,
+    website: string,
+    image: File | null,
+}
